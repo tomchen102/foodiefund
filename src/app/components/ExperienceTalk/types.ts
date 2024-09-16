eb971e564a0e106de9d578f9816f4993cfa3427d
Jethro <ttppoo12144@gmail.com>
2024-09-17 00:15:08 +1000

    feat:新增 首頁 募資提案經驗談 的區塊 和 新增 react-icons 插件 完成首頁 經驗談區塊 含 RWD輪播功能

---
+++ src/app/components/ExperienceTalk/types.ts
@@ -0,0 +1,12 @@
+export interface ExperienceTalkProps {
+  data: {
+    id: number;
+    title: string;
+    description: string;
+    imageUrl: string;
+    avatarUrl: string;
+    avatarAlt: string;
+    avatarName: string;
+    avatarRole: string;
+  }[];
+}
