0241fd053af75d5033db4b74991dd11a73a9a992
Jethro <ttppoo12144@gmail.com>
2024-09-30 18:17:58 +1000

    feat:新增 middleware 方法 用來判斷 路由守衛

---
+++ src/middleware.ts
@@ -0,0 +1,30 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export async function middleware(req: NextRequest) {
+  const tokenCookie = req.cookies.get("token");
+  const token = tokenCookie ? tokenCookie.value : null;
+
+  // 如果用戶已經登錄，並且嘗試訪問登錄頁面，則重定向到主頁
+  if (token && req.nextUrl.pathname === "/login") {
+    console.log("User already logged in, redirecting to home page");
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
+  // 如果用戶未登錄，並且嘗試訪問受保護的路由，則重定向到登錄頁面
+  if (!token && req.nextUrl.pathname.startsWith("/test")) {
+    console.log("No token, redirecting to login page");
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+
+  // 將 token 添加到請求頭中，傳遞給後端
+  if (token) {
+    req.headers.set("Authorization", `Bearer ${token}`);
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/test/:path*", "/login"], // 保護的路由和登錄頁面
+};
