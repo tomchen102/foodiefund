abebe7eb167cfd76f243964512945513b0ae0cc6
Jethro <ttppoo12144@gmail.com>
2024-10-01 22:34:49 +1000

    feat:新增 註冊 API 邏輯 / 把 DataResponse 拉出來 順便增強 API 泛型

--- src/hooks/useUserAuth/index.ts
+++ src/hooks/useUserAuth/index.ts
@@ -1,26 +1,31 @@
-import { signIn } from "@/api/services/LoginService";
-import { FormLoginSchemaType } from "@/schema/UserAuth";
+import { signIn, signUp } from "@/api/services/signFlow";
+import { DataResponse } from "@/api/services/signFlow/types";
+import { FormLoginSchemaType, FormRegisterSchemaType } from "@/schema/UserAuth";
 import { ErrorResponse } from "@/types/errorResponse";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, UseMutationResult } from "@tanstack/react-query";
 import { AxiosError, AxiosResponse } from "axios";
 
-export interface DataResponse {
-  data: {
-    name: string;
-    user: {
-      name: string;
-      photo: string;
-      token: string;
-    };
-  };
-  status: string;
-}
-
-export const useLogInMutation = () => {
+export const useLogInMutation = (): UseMutationResult<
+  AxiosResponse<DataResponse>,
+  AxiosError<ErrorResponse>,
+  FormLoginSchemaType
+> => {
   return useMutation<AxiosResponse<DataResponse>, AxiosError<ErrorResponse>, FormLoginSchemaType>({
     mutationFn: async (user: FormLoginSchemaType) => await signIn(user),
     onError: (error: AxiosError<ErrorResponse>) => {
-      console.log(123);
+      console.error("Error logging in user:", error.response?.data.message);
+    },
+  });
+};
+
+export const useRegisterMutation = (): UseMutationResult<
+  AxiosResponse<DataResponse>,
+  AxiosError<ErrorResponse>,
+  FormRegisterSchemaType
+> => {
+  return useMutation<AxiosResponse<DataResponse>, AxiosError<ErrorResponse>, FormRegisterSchemaType>({
+    mutationFn: async (user: FormRegisterSchemaType) => await signUp(user),
+    onError: (error: AxiosError<ErrorResponse>) => {
       console.error("Error logging in user:", error.response?.data.message);
     },
   });
