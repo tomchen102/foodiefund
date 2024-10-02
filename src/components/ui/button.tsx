3ce3e4b11ef7a9ee747b16441a1f29fae75c70f0
Jethro <ttppoo12144@gmail.com>
2024-10-02 22:53:12 +1000

    feat:新增 立即贊助 按鈕 樣式 variant為 donateNow size用lg 很多地方都會用到

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -16,11 +16,12 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80 text-white text-sm font-bold leading-[20px]",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
+        donateNow: "bg-destructive text-base font-bold text-white hover:bg-destructive/90",
       },
       size: {
         default: "h-9 px-4 py-2",
         sm: "h-8 rounded-md px-3 text-xs",
-        lg: "h-10 rounded-md px-8",
+        lg: "h-10 rounded py-6 w-full",
         icon: "h-9 w-9",
       },
     },
