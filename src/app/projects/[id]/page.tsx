bd18f03dd743c5d98a312051792c1dc156a58581
Jethro <ttppoo12144@gmail.com>
2024-10-04 20:57:56 +1000

    feat:修正 專案 頁面 架構 模擬靜態資料傳遞資料給專案概要

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -1,74 +1,89 @@
-import Badges from "@/components/Badges";
-import { Button } from "@/components/ui/button";
-import { Progress } from "@/components/ui/progress";
-import Link from "next/link";
-import { MdOutlineCalendarMonth, MdPersonOutline } from "react-icons/md";
+import RecentProjectsBlock from "@/components/RecentProjectsBlock";
+import ProjectSummary from "./_components/ProjectSummary";
 
-const projects = () => {
+export const getData = () => {
+  return [
+    {
+      id: 1,
+      title: "咖啡與時光",
+      description: "義式咖啡與甜點完美結合,是愛好者的溫馨聚會空間。",
+      imageUrl:
+        "https://storage.googleapis.com/fir-express-80358.appspot.com/images/74959d50-8101-4457-acfb-95ba81a0830b.png?GoogleAccessId=firebase-adminsdk-42otj%40fir-express-80358.iam.gserviceaccount.com&Expires=16756675200&Signature=HXFkUjTSFlvNcbPm3WlzMgzJCAkeZwKv5eGgoDX0CyTlpemku3TypoirUsdX3BXI2RSTzWJysA8vNXe6gV3CDsP%2Be8R%2B%2BQFetGyoXZm7NuOUq8P2RG4mToCzjMe4G%2BtHp4pmLKIWzSPVmULX8p0Yg%2F6GkifttKxmJh1S0JQJ3QgB9%2BbBIJ2Le73Hm7p%2BcftKUpdWy%2BxVVVM1dKfjK2b4eAfRbEhXwjqCdqrJfR4elZZtND8n%2F9nl%2BhlnDRAMlfqM9GAMuO7fWrpzOFvjZCdj28xd7bt0spMOGoOZCnfJOi%2BI5VZKU7xx8f6u%2FbbvLtJSGNILfVRBhtBpvi%2Fjr9ev7Q%3D%3D",
+      place: "台中",
+      percentage: 80,
+      targetAmount: "1,000,000",
+      reciprocal: 30,
+    },
+    {
+      id: 2,
+      title: "甜點夢工廠",
+      description: "法國甜點大師操刀,創造出經典與創意融合的法式糕點。",
+      imageUrl:
+        "https://storage.googleapis.com/fir-express-80358.appspot.com/images/473fad14-a22c-4a19-a9d7-47ec4dba1327.png?GoogleAccessId=firebase-adminsdk-42otj%40fir-express-80358.iam.gserviceaccount.com&Expires=16756675200&Signature=N%2FZXFgCu2oH3%2BcvVSp5wFJh7899RH395n1B5XG05NLtu6DuE8MHViAkUTNcnsAFTmmwmzC72NC65EDV1WUbIlZEpxvA41Gjeu7r0lJ0tT7b4tArP%2BPObjA1waRPAzSxZR%2FPPt9dfPefnbFPQk8eq1zJFhtKM5PxX0skMm7yVeTDObjrsxcxNK3rMrAKMXiAJH5tpsCt4eXZBcX5pi1ceKRR3qsgI4YL3RynOg3ghhtFgHiLV86pJU9VeSXWZEQ%2FNP5JTka5WrIwRsMyrkxG78IfgyflasitfPEu0JatpOfUs7zRyvT%2BXF0uA0193EaTMW3wgGZWiu7Cr76EUDkB%2B1A%3D%3D",
+      place: "台南",
+      percentage: 60,
+      targetAmount: "500,000",
+      reciprocal: 20,
+    },
+    {
+      id: 3,
+      title: "海岸燒烤樂園",
+      description: "在美麗海岸享受豐富的海鮮和肉類燒烤,體驗戶外用餐的樂趣。",
+      imageUrl:
+        "https://storage.googleapis.com/fir-express-80358.appspot.com/images/35cbb20a-335a-4a27-96be-f75b399f6d8d.png?GoogleAccessId=firebase-adminsdk-42otj%40fir-express-80358.iam.gserviceaccount.com&Expires=16756675200&Signature=v%2BT%2FBpxiJ1%2FUJ%2BrcaLBUZBTW1arqriZqvw5AoA0ZsA4izX0mu1cVEaZUV1Ga7x4x%2F8QdRY%2BjvcZV0fki4PQRiiNyhOuZ8hUd50YVYr9litu8fi8MB0aAKH8HxjMwRvtvDYnNMZM%2B4YHROhb4kT%2BSojWCCy%2FftU7g%2FLzvYcRGQZ5%2F6mEoo9U7jcBsYfv%2FjFx2RRLvuoi4F07NolYeLwFwA9VsW6l2pW0Su%2F6OYwcrvQge3yE4ct%2FMamUU9UhWsq8oNvA2t6urPwMd59J5ZAXZyeO4WvE0DSbTSn%2FUJXU9gRFYIpTw0ilNxAwj9C4jTYqefMSWhb9uD0wdepTas87jTw%3D%3D",
+      place: "台中",
+      percentage: 90,
+      targetAmount: "2,000,000",
+      reciprocal: 15,
+    },
+    {
+      id: 4,
+      title: "綠意盎然",
+      description: "綠意盎然的環境,提供健康的蔬食和飲品,是健康生活的首選。",
+      imageUrl:
+        "https://storage.googleapis.com/fir-express-80358.appspot.com/images/35cbb20a-335a-4a27-96be-f75b399f6d8d.png?GoogleAccessId=firebase-adminsdk-42otj%40fir-express-80358.iam.gserviceaccount.com&Expires=16756675200&Signature=v%2BT%2FBpxiJ1%2FUJ%2BrcaLBUZBTW1arqriZqvw5AoA0ZsA4izX0mu1cVEaZUV1Ga7x4x%2F8QdRY%2BjvcZV0fki4PQRiiNyhOuZ8hUd50YVYr9litu8fi8MB0aAKH8HxjMwRvtvDYnNMZM%2B4YHROhb4kT%2BSojWCCy%2FftU7g%2FLzvYcRGQZ5%2F6mEoo9U7jcBsYfv%2FjFx2RRLvuoi4F07NolYeLwFwA9VsW6l2pW0Su%2F6OYwcrvQge3yE4ct%2FMamUU9UhWsq8oNvA2t6urPwMd59J5ZAXZyeO4WvE0DSbTSn%2FUJXU9gRFYIpTw0ilNxAwj9C4jTYqefMSWhb9uD0wdepTas87jTw%3D%3D",
+      place: "台北",
+      percentage: 60,
+      targetAmount: "500,000",
+      reciprocal: 20,
+    },
+  ];
+};
+
+export const getProjectSummaryData = () => {
+  return {
+    id: "1",
+    title: "喵喵咖啡廳",
+    description:
+      "喵喵咖啡館在於創辦人在台北時創立,我們的理念很簡單-提供優質的食物和咖啡,咖啡具有使人們停下來和彼此互動的能力。人與人之間的聯繫就是我們在這裡的原因,有時我們意識到這就像兩個拿鐵一樣簡單。享用咖啡,早餐或午餐,我們隨時為您服務!",
+    imageUrlLg:
+      "https://storage.googleapis.com/fir-express-80358.appspot.com/images/4205b53b-bb9b-4d16-83d8-863b2f0f6fb8.png?GoogleAccessId=firebase-adminsdk-42otj%40fir-express-80358.iam.gserviceaccount.com&Expires=16756675200&Signature=E8hfbipNltmZrQfCOGYMclvdz9hQHrXyfofrvYpepc9d3kQgT%2FjFPXN8GIluV7oO6T0m9jEQ3lGyFLC3M%2FNlEs10zB4SXWj5WeviojCP846SRxXIzXAjnJwkFuhZGBqjuyrX0E9L5DoFBhHWYQqnxFblCjqo96dR6FhKJSg2luTqHyFSRtWe8kBFIlFR2Ycu09TSsgOVSG%2BMnCjl734mjkAmjOR36S9rfTG5RBbTSvYvHvRKuLwl437oxglHTEtydzhJKAW9HfIDdvp1MNuYkJ8f5X6%2Fs1cCfWGUYHiJaOqDncGUYObSEyHLAyj1xUSwu0l2WFR%2BwRcFjOM9f%2FXeGg%3D%3D",
+    imageUrlSm:
+      "https://storage.googleapis.com/fir-express-80358.appspot.com/images/74960e77-27bb-4145-9c74-70c5c5b3c207.png?GoogleAccessId=firebase-adminsdk-42otj%40fir-express-80358.iam.gserviceaccount.com&Expires=16756675200&Signature=PXtAq0TxkxLZMbGU1xWId8nHIEr9XEVfESbkhydEs0zzFn5x1rMLcDzUMRk%2BMyUkVSCtCow%2F8he37%2F8gbfU9NFGH7gUaoBQatqFQZwGwm4HuNXjPK5oVoxRA5IJRoV4k%2FvDyq3g51wmsC55mIqDxV5cWWPfaJMIKi3LFgKlKwXeNoN%2FqVeTE3OqbjvcGkRBV%2FylbgEqTgnG4keVpwzS2%2BhFIezUxUOWDyY23b%2FbMNLV9a9vXDvDWFsvcdeB0GkAW842rz5%2F9crT%2BJTKTnp6A2ViONMLMR743oUNA1597Cu9ZaIProsdR8nQfb7m97kNvyg81lfvh9oBBaL4c8ql7%2Fw%3D%3D",
+    proposer: "兔寶寶溜滑梯",
+    progress: 37,
+    targetAmount: "1,000,000",
+    currentAmount: "800,000",
+    startDate: "2022-08-01",
+    endDate: "13",
+    numberOfBackers: 115,
+    location: "台北",
+    restaurantType: "咖啡廳",
+    socialLinks: {
+      facebook: "https://www.facebook.com/",
+      twitter: "https://twitter.com/",
+      instagram: "https://www.instagram.com/",
+    },
+  };
+};
+
+const projects = async () => {
+  const data = await getData();
+  const ProjectSummaryData = await getProjectSummaryData();
   return (
-    <div className="container lg:pb-[120px] lg:pt-20 xl:px-0">
-      <div className="lg:grid lg:grid-cols-12 lg:gap-10">
-        {/* lg:w-[550px] */}
-        <div className="h-60 bg-[url('https://storage.googleapis.com/fir-express-80358.appspot.com/images/1ec19f5f-6e26-45dd-b1d1-2277de8248f2.png?GoogleAccessId=firebase-adminsdk-42otj%40fir-express-80358.iam.gserviceaccount.com&Expires=16756675200&Signature=Q16xulxexuuA4xIPC%2FE%2BRdZID4swLMTFbqeB38az%2F0Sq8sW4pri7uZK88R1UGBkdHbmuJgYIQbcej%2FHPcjl8P9UcMAefwfsahG41aEyCg3YM%2BYrEvINK9JeXMCN2JRcmMr%2BRkiQrqWVkI78OnvA187B87u4GiBGnqxbhezXDXdjuPJEUAWMClrlsodA1hRqlWFLYtEIYSb9rm9OPRd5EiLV1mb7XlAEHHUvB7JxpTYCiYPdJtgRmxetoG74Uf%2BVz65ysNg6iHEba%2FTmNSG6h0hCax4M21GL%2FZfNY6E7aoY7ojGGreeH4JoV%2Fot%2BBah4%2BjrYV1xJCQ3tALNVsOPIcaw%3D%3D')] bg-cover bg-center lg:col-span-5 lg:h-[550px] lg:bg-[url('https://storage.googleapis.com/fir-express-80358.appspot.com/images/74960e77-27bb-4145-9c74-70c5c5b3c207.png?GoogleAccessId=firebase-adminsdk-42otj%40fir-express-80358.iam.gserviceaccount.com&Expires=16756675200&Signature=PXtAq0TxkxLZMbGU1xWId8nHIEr9XEVfESbkhydEs0zzFn5x1rMLcDzUMRk%2BMyUkVSCtCow%2F8he37%2F8gbfU9NFGH7gUaoBQatqFQZwGwm4HuNXjPK5oVoxRA5IJRoV4k%2FvDyq3g51wmsC55mIqDxV5cWWPfaJMIKi3LFgKlKwXeNoN%2FqVeTE3OqbjvcGkRBV%2FylbgEqTgnG4keVpwzS2%2BhFIezUxUOWDyY23b%2FbMNLV9a9vXDvDWFsvcdeB0GkAW842rz5%2F9crT%2BJTKTnp6A2ViONMLMR743oUNA1597Cu9ZaIProsdR8nQfb7m97kNvyg81lfvh9oBBaL4c8ql7%2Fw%3D%3D')]"></div>
-        <section className="container m-0 px-3 pt-4 lg:col-span-7 lg:p-0">
-          {/* lg:w-[706px] */}
-          <div className="flex h-full flex-col">
-            <div>
-              <Badges text="台北" />
-              <Badges text="咖啡廳" />
-            </div>
-            <h1 className="my-3 text-2xl font-bold lg:text-4xl">喵喵咖啡廳</h1>
-            <p className="mb-6 text-sm font-medium lg:mb-auto lg:text-base">
-              喵喵咖啡館在於創辦人在台北時創立,我們的理念很簡單-提供優質的食物和咖啡,
-              咖啡具有使人們停下來和彼此互動的能力。人與人之間的聯繫就是我們在這裡的原因,有時我們意識到這就像兩個拿鐵一樣簡單。
-              享用咖啡,早餐或午餐,我們隨時為您服務!
-            </p>
-            <div className="mb-6 lg:mb-10 lg:flex lg:items-center">
-              <div className="mb-1 flex h-[30px] items-center lg:mb-0 lg:mr-5">
-                <MdPersonOutline size={20} className="mr-1 inline-block" />
-                <span className="mr-1 text-sm font-medium text-[#71717A]">提案人</span>
-                <span className="text-sm font-medium text-[#0F514E]">
-                  <Link href="#">兔寶寶溜滑梯</Link>
-                </span>
-              </div>
-              <div className="flex h-[30px] items-center">
-                <MdOutlineCalendarMonth size={20} className="mr-1 inline-block" />
-                <span className="mr-1 text-sm font-medium text-[#71717A]">開始於</span>
-                <span className="text-sm font-medium text-[#0F514E]">
-                  <time dateTime="2024/08/08 22:06">2024/08/08 22:06</time>
-                </span>
-              </div>
-            </div>
-            <div className="mb-7 lg:mb-10 lg:flex lg:gap-6">
-              <div className="lg:w-[426px]">
-                <div className="flex">
-                  <span className="mr-auto font-medium leading-6 text-[#71717A]">目標 NT$ 3,000,000</span>
-                  <span className="text-sm font-bold text-[#0F514E]">30%</span>
-                </div>
-                <Progress value={33} className="my-5" />
-                <div className="mb-6">
-                  <span className="text-2xl font-bold text-secondary lg:text-[30px] lg:leading-9">NT$ 123,456</span>
-                </div>
-              </div>
-              <div className="flex gap-4 lg:w-[256px]">
-                <div className="flex flex-grow flex-col items-center rounded-lg border p-2 lg:py-[14px]">
-                  <span className="mb-3 block text-sm font-medium text-[#27272A] lg:mb-5 lg:text-base">募資倒數</span>
-                  <span className="block text-2xl font-bold text-[#0F514E] lg:text-[30px] lg:leading-9">15 天</span>
-                </div>
-                <div className="flex flex-grow flex-col items-center rounded-lg border p-2 lg:py-[14px]">
-                  <span className="mb-3 block text-sm font-medium text-[#27272A] lg:mb-5 lg:text-base">贊助人數</span>
-                  <span className="block text-2xl font-bold text-[#0F514E] lg:text-[30px] lg:leading-9">661人</span>
-                </div>
-              </div>
-            </div>
-            <div className="mb-8 lg:mb-0">
-              <Button variant="donateNow" size="lg" asChild>
-                <Link href="#">立即贊助</Link>
-              </Button>
-            </div>
-          </div>
-        </section>
-      </div>
+    <div>
+      <ProjectSummary ProjectSummaryData={ProjectSummaryData} />
+      <RecentProjectsBlock className="bg-[#F5E5CE]" data={data} title="近期專案" />
     </div>
   );
 };
