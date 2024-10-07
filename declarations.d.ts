859b3b44382d097940b3dc2b90b6abcca20c93a5
Jethro <ttppoo12144@gmail.com>
2024-10-07 22:46:39 +1100

    chore: 新增 TS 聲明檔 declare module "*.module.css" declarations.d.ts 新增 css in js 聲明檔

--- declarations.d.ts
+++ declarations.d.ts
@@ -1,3 +1,8 @@
 // NOTE: The declaration below was injected by `"framer"`
 // see https://www.framer.com/docs/guides/handshake for more information.
 declare module "https://framer.com/m/*";
+
+declare module "*.module.css" {
+  const classes: { [key: string]: string };
+  export default classes;
+}
