43720fc6b77c10a2316124961634037410a50f18
Jethro <ttppoo12144@gmail.com>
2024-10-14 01:16:46 +1100

    feat: 新增 SectionPadding 組件 和 使用 固定  Section pb-8 pt-6 lg:mt-[100px] lg:gap-6 lg:pb-[120px] 模組化

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import RegisterForm from "./_components/RegisterForm";
+import SectionPadding from "@/components/SectionPadding";
 
 const Register = () => {
   return (
-    <section className="container px-3 pb-[120px] pt-20 xl:px-0">
+    <SectionPadding className="container px-3 xl:px-0">
       <div className="rounded-xl border border-black bg-white p-6 md:mx-auto md:w-[636px] md:p-10">
         <RegisterForm />
       </div>
-    </section>
+    </SectionPadding>
   );
 };
 
