7f500c99dd2767c1a4c5abb418fccbde14d39659
Jethro <ttppoo12144@gmail.com>
2024-10-14 20:07:22 +1100

    style: 修正 SectionPadding 的 class 原本是mt 改成 pt

--- src/components/SectionPadding/index.tsx
+++ src/components/SectionPadding/index.tsx
@@ -2,7 +2,7 @@ import { cn } from "@/lib/utils";
 import { ProviderProps } from "@/types/ProviderType";
 
 const SectionPadding = ({ children, className }: ProviderProps) => {
-  return <section className={cn("pb-8 pt-6 lg:mt-[100px] lg:gap-6 lg:pb-[120px]", className)}>{children}</section>;
+  return <section className={cn("pb-8 pt-6 lg:gap-6 lg:pb-[120px] lg:pt-[100px]", className)}>{children}</section>;
 };
 
 export default SectionPadding;
