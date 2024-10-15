a5e8acae6e99cd1d602b00a21220d6db7de7b039
Jethro <ttppoo12144@gmail.com>
2024-10-15 23:16:11 +1100

    feat: 保護 /payment-successful 路由 登入後才能訪問到 付款成功

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,13 +6,18 @@ export async function middleware(req: NextRequest) {
   const token = tokenCookie ? tokenCookie.value : null;
 
   // 如果用戶已經登錄，並且嘗試訪問登錄或註冊頁面，則重定向到主頁
-  if (token && (req.nextUrl.pathname === "/login" || req.nextUrl.pathname === "/register")) {
+  if (
+    token &&
+    (req.nextUrl.pathname === "/login" ||
+      req.nextUrl.pathname === "/register" ||
+      req.nextUrl.pathname === "/payment-successful")
+  ) {
     console.log("User already logged in, redirecting to home page");
     return NextResponse.redirect(new URL("/", req.url));
   }
 
   // 如果用戶未登錄，並且嘗試訪問受保護的路由，則重定向到登錄頁面
-  if (!token && req.nextUrl.pathname.startsWith("/test")) {
+  if (!token && (req.nextUrl.pathname.startsWith("/test") || req.nextUrl.pathname === "/payment-successful")) {
     console.log("No token, redirecting to login page");
     return NextResponse.redirect(new URL("/login", req.url));
   }
@@ -26,5 +31,5 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/test/:path*", "/login", "/register"], // 保護的路由和登錄頁面
+  matcher: ["/test/:path*", "/login", "/register", "/payment-successful"], // 保護的路由和登錄頁面
 };
