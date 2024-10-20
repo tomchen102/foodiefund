b182804c44b6c85ed603b6ece9de8808578211c7
Lobinda <lobinda123@gmail.com>
2024-10-20 14:32:16 +0800

    test getURL

--- src/app/redirect/page.tsx
+++ src/app/redirect/page.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 
 import SectionPadding from "@/components/SectionPadding";
-
+import RedirectPage from "./_components/RedirectPage";
 const Register = () => {
   return (
     <SectionPadding className="container px-3 xl:px-0">
-      <div className="rounded-xl border border-black bg-white p-6 md:mx-auto md:w-[636px] md:p-10">123</div>
+      <RedirectPage></RedirectPage>
     </SectionPadding>
   );
 };
