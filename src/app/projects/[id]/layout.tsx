b9c06cf2ba89adba7ac3f89cc7bc2aba9a637ccc
Jethro <ttppoo12144@gmail.com>
2024-10-20 20:12:34 +1100

    feat:新增 SEO 專案內容 新增 metadata 資訊

--- src/app/projects/[id]/layout.tsx
+++ src/app/projects/[id]/layout.tsx
@@ -86,6 +86,33 @@ export const metadata: Metadata = {
   title: "眾資成城-在你心愛的餐廳成為合夥人",
   description:
     "在開發「眾資成城 - 在你心愛的餐廳成為合夥人」這個網站時，我們的理念是創造一個聚焦於美食愛好者和創業家之間合作的平台。這個網站旨在促進社區內外的餐廳發展，讓顧客不僅僅是消費者，更能成為餐廳成長的重要合作夥伴。首先，我們將提供一個直觀易用的募資平台，讓想要投資或支持餐廳發展的人能夠與餐廳業主直接溝通和合作。這不僅包括資金支持，還包括推廣、建議和體驗分享。其次，網站將建立一個社區氛圍，讓餐廳業主能夠與顧客建立密切的關係。這將透過討論區、社交媒體整合和專屬會員活動來實現，使得顧客不僅是消費者，更是餐廳成功的一部分。此外，我們將提供豐富的資源和工具，包括市場分析、行業洞察和專家建議，幫助餐廳業主做出明智的決策，並在競爭激烈的市場中保持競爭力。最後，我們將通過故事分享和成功案例來鼓勵和激勵更多人參與到餐廳合夥人計畫中，這些故事將展示不同餐廳和合夥人之間的合作模式和成就，啟發更多類似項目的創建和支持。總體而言，「眾資成城」將不僅僅是一個募資平台，更是一個社區聚集地，鼓勵和支持餐廳創業家們實現他們的夢想，同時讓顧客能夠深入參與到這個過程中，共同見證和分享美食和成功的喜悅。",
+  keywords: "眾資成城, 餐廳, 合夥人, 群眾募資, 美食",
+  authors: [{ name: "你的名字" }],
+  viewport: "width=device-width, initial-scale=1",
+  robots: "index, follow",
+  openGraph: {
+    title: "眾資成城-在你心愛的餐廳成為合夥人",
+    description:
+      "在開發「眾資成城 - 在你心愛的餐廳成為合夥人」這個網站時，我們的理念是創造一個聚焦於美食愛好者和創業家之間合作的平台。",
+    url: "https://foodiefund.vercel.app/projects/123",
+    type: "website",
+    images: [
+      {
+        url: "https://storage.googleapis.com/fir-express-80358.appspot.com/images/74960e77-27bb-4145-9c74-70c5c5b3c207.png?GoogleAccessId=firebase-adminsdk-42otj%40fir-express-80358.iam.gserviceaccount.com&Expires=16756675200&Signature=PXtAq0TxkxLZMbGU1xWId8nHIEr9XEVfESbkhydEs0zzFn5x1rMLcDzUMRk%2BMyUkVSCtCow%2F8he37%2F8gbfU9NFGH7gUaoBQatqFQZwGwm4HuNXjPK5oVoxRA5IJRoV4k%2FvDyq3g51wmsC55mIqDxV5cWWPfaJMIKi3LFgKlKwXeNoN%2FqVeTE3OqbjvcGkRBV%2FylbgEqTgnG4keVpwzS2%2BhFIezUxUOWDyY23b%2FbMNLV9a9vXDvDWFsvcdeB0GkAW842rz5%2F9crT%2BJTKTnp6A2ViONMLMR743oUNA1597Cu9ZaIProsdR8nQfb7m97kNvyg81lfvh9oBBaL4c8ql7%2Fw%3D%3D",
+        width: 800,
+        height: 600,
+        alt: "Og Image Alt",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "眾資成城-在你心愛的餐廳成為合夥人",
+    description:
+      "在開發「眾資成城 - 在你心愛的餐廳成為合夥人」這個網站時，我們的理念是創造一個聚焦於美食愛好者和創業家之間合作的平台。",
+    images:
+      "https://storage.googleapis.com/fir-express-80358.appspot.com/images/74960e77-27bb-4145-9c74-70c5c5b3c207.png?GoogleAccessId=firebase-adminsdk-42otj%40fir-express-80358.iam.gserviceaccount.com&Expires=16756675200&Signature=PXtAq0TxkxLZMbGU1xWId8nHIEr9XEVfESbkhydEs0zzFn5x1rMLcDzUMRk%2BMyUkVSCtCow%2F8he37%2F8gbfU9NFGH7gUaoBQatqFQZwGwm4HuNXjPK5oVoxRA5IJRoV4k%2FvDyq3g51wmsC55mIqDxV5cWWPfaJMIKi3LFgKlKwXeNoN%2FqVeTE3OqbjvcGkRBV%2FylbgEqTgnG4keVpwzS2%2BhFIezUxUOWDyY23b%2FbMNLV9a9vXDvDWFsvcdeB0GkAW842rz5%2F9crT%2BJTKTnp6A2ViONMLMR743oUNA1597Cu9ZaIProsdR8nQfb7m97kNvyg81lfvh9oBBaL4c8ql7%2Fw%3D%3D",
+  },
 };
 
 export default async function ProjectsLayout({ children }: ProviderProps) {
@@ -93,6 +120,23 @@ export default async function ProjectsLayout({ children }: ProviderProps) {
   const ProjectSummaryData = await getProjectSummaryData();
   metadata.title = ProjectSummaryData.title;
   metadata.description = ProjectSummaryData.description;
+  metadata.openGraph = metadata.openGraph ?? {};
+  metadata.openGraph.title = ProjectSummaryData.title;
+  metadata.openGraph.description = ProjectSummaryData.description;
+  metadata.openGraph.url = "https://foodiefund.vercel.app/projects/123";
+  metadata.openGraph.images = [
+    {
+      url: "https://storage.googleapis.com/fir-express-80358.appspot.com/images/74960e77-27bb-4145-9c74-70c5c5b3c207.png?GoogleAccessId=firebase-adminsdk-42otj%40fir-express-80358.iam.gserviceaccount.com&Expires=16756675200&Signature=PXtAq0TxkxLZMbGU1xWId8nHIEr9XEVfESbkhydEs0zzFn5x1rMLcDzUMRk%2BMyUkVSCtCow%2F8he37%2F8gbfU9NFGH7gUaoBQatqFQZwGwm4HuNXjPK5oVoxRA5IJRoV4k%2FvDyq3g51wmsC55mIqDxV5cWWPfaJMIKi3LFgKlKwXeNoN%2FqVeTE3OqbjvcGkRBV%2FylbgEqTgnG4keVpwzS2%2BhFIezUxUOWDyY23b%2FbMNLV9a9vXDvDWFsvcdeB0GkAW842rz5%2F9crT%2BJTKTnp6A2ViONMLMR743oUNA1597Cu9ZaIProsdR8nQfb7m97kNvyg81lfvh9oBBaL4c8ql7%2Fw%3D%3D",
+      width: 800,
+      height: 600,
+      alt: "Og Image Alt",
+    },
+  ];
+  metadata.twitter = metadata.twitter ?? {};
+  metadata.twitter.title = ProjectSummaryData.title;
+  metadata.twitter.description = ProjectSummaryData.description;
+  metadata.twitter.images =
+    "https://storage.googleapis.com/fir-express-80358.appspot.com/images/74960e77-27bb-4145-9c74-70c5c5b3c207.png?GoogleAccessId=firebase-adminsdk-42otj%40fir-express-80358.iam.gserviceaccount.com&Expires=16756675200&Signature=PXtAq0TxkxLZMbGU1xWId8nHIEr9XEVfESbkhydEs0zzFn5x1rMLcDzUMRk%2BMyUkVSCtCow%2F8he37%2F8gbfU9NFGH7gUaoBQatqFQZwGwm4HuNXjPK5oVoxRA5IJRoV4k%2FvDyq3g51wmsC55mIqDxV5cWWPfaJMIKi3LFgKlKwXeNoN%2FqVeTE3OqbjvcGkRBV%2FylbgEqTgnG4keVpwzS2%2BhFIezUxUOWDyY23b%2FbMNLV9a9vXDvDWFsvcdeB0GkAW842rz5%2F9crT%2BJTKTnp6A2ViONMLMR743oUNA1597Cu9ZaIProsdR8nQfb7m97kNvyg81lfvh9oBBaL4c8ql7%2Fw%3D%3D";
   return (
     <>
       <ProjectSummary ProjectSummaryData={ProjectSummaryData} />
