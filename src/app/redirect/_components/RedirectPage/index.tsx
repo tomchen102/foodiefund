fbad59ad0849e94891fb0a909db599b30adb20e9
Lobinda <lobinda123@gmail.com>
2024-10-20 16:07:34 +0800

    test:getURL

--- src/app/redirect/_components/RedirectPage/index.tsx
+++ src/app/redirect/_components/RedirectPage/index.tsx
@@ -5,7 +5,7 @@ import { useSearchParams, useRouter } from "next/navigation";
 import Cookies from "js-cookie";
 import { useAuth } from "@/utils/providers/AuthProvider";
 
-const RedirectPage = () => {
+const RedirectPageContent = () => {
   const searchParams = useSearchParams();
   const router = useRouter();
   const { setUser } = useAuth();
@@ -20,16 +20,24 @@ const RedirectPage = () => {
       Cookies.set("token", JSON.stringify({ name, photo, token }));
       setUser({ name, photo, token });
       setTimeout(() => {
-        // router.push("/");
+        router.push("/");
       }, 2000);
     }
   }, [searchParams, router, setUser]);
 
   return (
-    <Suspense>
+    <div>
       {/* Display the extracted search parameters */}
       <p>Token: {searchParams.get("token")}</p>
       <p>Name: {searchParams.get("name")}</p>
+    </div>
+  );
+};
+
+const RedirectPage = () => {
+  return (
+    <Suspense fallback={<div>Loading....</div>}>
+      <RedirectPageContent></RedirectPageContent>
     </Suspense>
   );
 };
