b6448d2b052068a19d48ee5344f03615b1bc1829
Jethro <ttppoo12144@gmail.com>
2024-10-21 16:26:21 +1100

    feat: 修改 lang 為 ZH 並增加 metadata 增加SEO 和 metadata 的 template

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import HeaderMenu from "./_components/Header";
 import FooterMenu from "./_components/Footer";
 import { AuthProvider } from "@/utils/providers/AuthProvider";
 import { ProviderProps } from "@/types/ProviderType";
+import { Metadata } from "next";
 
 const GeistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -19,15 +20,57 @@ const GeistMono = localFont({
   weight: "100 900",
 });
 
-export const metadata = {
-  title: "眾資成城-在你心愛的餐廳成為合夥人",
+export const metadata: Metadata = {
+  title: {
+    absolute: "眾資成城-在你心愛的餐廳成為合夥人",
+    template: "%s | 眾資成城-在你心愛的餐廳成為合夥人",
+  },
   description:
     "在開發「眾資成城 - 在你心愛的餐廳成為合夥人」這個網站時，我們的理念是創造一個聚焦於美食愛好者和創業家之間合作的平台。這個網站旨在促進社區內外的餐廳發展，讓顧客不僅僅是消費者，更能成為餐廳成長的重要合作夥伴。首先，我們將提供一個直觀易用的募資平台，讓想要投資或支持餐廳發展的人能夠與餐廳業主直接溝通和合作。這不僅包括資金支持，還包括推廣、建議和體驗分享。其次，網站將建立一個社區氛圍，讓餐廳業主能夠與顧客建立密切的關係。這將透過討論區、社交媒體整合和專屬會員活動來實現，使得顧客不僅是消費者，更是餐廳成功的一部分。此外，我們將提供豐富的資源和工具，包括市場分析、行業洞察和專家建議，幫助餐廳業主做出明智的決策，並在競爭激烈的市場中保持競爭力。最後，我們將通過故事分享和成功案例來鼓勵和激勵更多人參與到餐廳合夥人計畫中，這些故事將展示不同餐廳和合夥人之間的合作模式和成就，啟發更多類似項目的創建和支持。總體而言，「眾資成城」將不僅僅是一個募資平台，更是一個社區聚集地，鼓勵和支持餐廳創業家們實現他們的夢想，同時讓顧客能夠深入參與到這個過程中，共同見證和分享美食和成功的喜悅。",
+  keywords: "眾資成城, 餐廳, 合夥人, 群眾募資, 美食",
+  openGraph: {
+    title: {
+      absolute: "眾資成城-在你心愛的餐廳成為合夥人",
+      template: "%s | 眾資成城-在你心愛的餐廳成為合夥人",
+    },
+    description:
+      "在開發「眾資成城 - 在你心愛的餐廳成為合夥人」這個網站時，我們的理念是創造一個聚焦於美食愛好者和創業家之間合作的平台。這個網站旨在促進社區內外的餐廳發展，讓顧客不僅僅是消費者，更能成為餐廳成長的重要合作夥伴。首先，我們將提供一個直觀易用的募資平台，讓想要投資或支持餐廳發展的人能夠與餐廳業主直接溝通和合作。這不僅包括資金支持，還包括推廣、建議和體驗分享。其次，網站將建立一個社區氛圍，讓餐廳業主能夠與顧客建立密切的關係。這將透過討論區、社交媒體整合和專屬會員活動來實現，使得顧客不僅是消費者，更是餐廳成功的一部分。此外，我們將提供豐富的資源和工具，包括市場分析、行業洞察和專家建議，幫助餐廳業主做出明智的決策，並在競爭激烈的市場中保持競爭力。最後，我們將通過故事分享和成功案例來鼓勵和激勵更多人參與到餐廳合夥人計畫中，這些故事將展示不同餐廳和合夥人之間的合作模式和成就，啟發更多類似項目的創建和支持。總體而言，「眾資成城」將不僅僅是一個募資平台，更是一個社區聚集地，鼓勵和支持餐廳創業家們實現他們的夢想，同時讓顧客能夠深入參與到這個過程中，共同見證和分享美食和成功的喜悅。",
+    type: "website",
+    siteName: "眾資成城-在你心愛的餐廳成為合夥人",
+    images: [
+      {
+        url: "https://storage.googleapis.com/fir-express-80358.appspot.com/images/48b107cd-e6c2-4337-9455-d3b93fa6c585.jpg?GoogleAccessId=firebase-adminsdk-42otj%40fir-express-80358.iam.gserviceaccount.com&Expires=16756675200&Signature=g7sjuCrnykNM2Zcvsh%2BouQDEjPCVwzzvcNHLTa%2BXw7K4dYK9HBpSQx3UaZ%2FWhdF8NMRANIMBxM%2FQXTKnNZZ8%2BkWUs1qGoZ0tFrOH6u6OD1YjxAxLtDFZZdNfEWPF10C6NzyxCD9lA7TfsL4mn8vUQ%2FHmQEcLV%2BJoxwJ3YgQ15szDCT%2FreKUO4VXTSRht9mdrGiM544r9Wn%2BEEHuFnExldqABB8TJ6KUPwKEe8Jlq8ss5EOoGHdF%2BuLWBv8oTVJAUa%2BQLdN8rYF8wbEhUlGc53CtO7ed8z4dghco9IXCMOjaWfJ1tejcpIaa9bi6zviAFvFY7mcilYLRWyY9QL4PwFw%3D%3D",
+        width: 800,
+        height: 600,
+        alt: "Og Image Alt",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: {
+      absolute: "眾資成城-在你心愛的餐廳成為合夥人",
+      template: "%s | 眾資成城-在你心愛的餐廳成為合夥人",
+    },
+    site: "@site",
+    description:
+      "在開發「眾資成城 - 在你心愛的餐廳成為合夥人」這個網站時，我們的理念是創造一個聚焦於美食愛好者和創業家之間合作的平台。這個網站旨在促進社區內外的餐廳發展，讓顧客不僅僅是消費者，更能成為餐廳成長的重要合作夥伴。首先，我們將提供一個直觀易用的募資平台，讓想要投資或支持餐廳發展的人能夠與餐廳業主直接溝通和合作。這不僅包括資金支持，還包括推廣、建議和體驗分享。其次，網站將建立一個社區氛圍，讓餐廳業主能夠與顧客建立密切的關係。這將透過討論區、社交媒體整合和專屬會員活動來實現，使得顧客不僅是消費者，更是餐廳成功的一部分。此外，我們將提供豐富的資源和工具，包括市場分析、行業洞察和專家建議，幫助餐廳業主做出明智的決策，並在競爭激烈的市場中保持競爭力。最後，我們將通過故事分享和成功案例來鼓勵和激勵更多人參與到餐廳合夥人計畫中，這些故事將展示不同餐廳和合夥人之間的合作模式和成就，啟發更多類似項目的創建和支持。總體而言，「眾資成城」將不僅僅是一個募資平台，更是一個社區聚集地，鼓勵和支持餐廳創業家們實現他們的夢想，同時讓顧客能夠深入參與到這個過程中，共同見證和分享美食和成功的喜悅。",
+    images:
+      "https://storage.googleapis.com/fir-express-80358.appspot.com/images/48b107cd-e6c2-4337-9455-d3b93fa6c585.jpg?GoogleAccessId=firebase-adminsdk-42otj%40fir-express-80358.iam.gserviceaccount.com&Expires=16756675200&Signature=g7sjuCrnykNM2Zcvsh%2BouQDEjPCVwzzvcNHLTa%2BXw7K4dYK9HBpSQx3UaZ%2FWhdF8NMRANIMBxM%2FQXTKnNZZ8%2BkWUs1qGoZ0tFrOH6u6OD1YjxAxLtDFZZdNfEWPF10C6NzyxCD9lA7TfsL4mn8vUQ%2FHmQEcLV%2BJoxwJ3YgQ15szDCT%2FreKUO4VXTSRht9mdrGiM544r9Wn%2BEEHuFnExldqABB8TJ6KUPwKEe8Jlq8ss5EOoGHdF%2BuLWBv8oTVJAUa%2BQLdN8rYF8wbEhUlGc53CtO7ed8z4dghco9IXCMOjaWfJ1tejcpIaa9bi6zviAFvFY7mcilYLRWyY9QL4PwFw%3D%3D",
+  },
+  appleWebApp: {
+    title: "眾資成城-在你心愛的餐廳成為合夥人",
+    startupImage: [
+      {
+        url: "https://storage.googleapis.com/fir-express-80358.appspot.com/images/48b107cd-e6c2-4337-9455-d3b93fa6c585.jpg?GoogleAccessId=firebase-adminsdk-42otj%40fir-express-80358.iam.gserviceaccount.com&Expires=16756675200&Signature=g7sjuCrnykNM2Zcvsh%2BouQDEjPCVwzzvcNHLTa%2BXw7K4dYK9HBpSQx3UaZ%2FWhdF8NMRANIMBxM%2FQXTKnNZZ8%2BkWUs1qGoZ0tFrOH6u6OD1YjxAxLtDFZZdNfEWPF10C6NzyxCD9lA7TfsL4mn8vUQ%2FHmQEcLV%2BJoxwJ3YgQ15szDCT%2FreKUO4VXTSRht9mdrGiM544r9Wn%2BEEHuFnExldqABB8TJ6KUPwKEe8Jlq8ss5EOoGHdF%2BuLWBv8oTVJAUa%2BQLdN8rYF8wbEhUlGc53CtO7ed8z4dghco9IXCMOjaWfJ1tejcpIaa9bi6zviAFvFY7mcilYLRWyY9QL4PwFw%3D%3D",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({ children }: ProviderProps) {
   return (
-    <html lang="en">
+    <html lang="zh">
       <body
         className={`${GeistSans.variable} ${GeistMono.variable} flex min-h-screen flex-col antialiased`}
         suppressHydrationWarning={true}
