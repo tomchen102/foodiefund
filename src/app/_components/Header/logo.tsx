91b150b8ffa15768e4d014c378eed51fea94e007
Lobinda <lobinda123@gmail.com>
2024-10-21 21:04:29 +0800

    fixed url

--- src/app/_components/Header/logo.tsx
+++ src/app/_components/Header/logo.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const Logo = () => {
   return (
-    <a href="index.html" className="logo-link mr-auto flex items-center p-3 font-bold">
+    <a href="/" className="logo-link mr-auto flex items-center p-3 font-bold">
       <div className="relative mr-2 inline-block h-6 w-6">
         <Image
           src="https://firebasestorage.googleapis.com/v0/b/foodiefund-7b103.appspot.com/o/logo.png?alt=media&token=50bbd619-ec49-441c-9030-501c4826fb69"
