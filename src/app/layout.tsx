6b8551e9f745f6d124308706fd52a7149fc950aa
Lobinda <lobinda123@gmail.com>
2024-10-26 15:18:16 +0800

    fixed layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -82,7 +82,7 @@ export default function RootLayout({ children }: ProviderProps) {
           <ReactQueryProvider>
             <LoadingProvider>
               <HeaderMenu />
-              <main className="flex flex-1 items-center justify-center">{children}</main>
+              <main className="block">{children}</main>
               <FooterMenu />
             </LoadingProvider>
           </ReactQueryProvider>
