5bf9d476f9c109e138d4b29bd0416ad63db4ace4
Jethro <ttppoo12144@gmail.com>
2024-10-27 15:37:35 +1100

    refactor:重購 表單 渲染 組件 把不同元件包裝後引入 更清晰

--- src/components/FormRenderer/index.tsx
+++ src/components/FormRenderer/index.tsx
@@ -1,57 +1,56 @@
-import React from "react";
-import { FieldValues, Path } from "react-hook-form";
-import { FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "../ui/form";
-import { Checkbox } from "../ui/checkbox";
-import { FormRendererProps } from "./types";
-import { Input } from "../ui/input";
-import { cn } from "@/lib/utils";
-import { Textarea } from "../ui/textarea";
+import { FieldValues, FormProvider } from "react-hook-form";
+import FormInput from "./FormInput";
+import { FormRendererProps, FormFieldConfig } from "./types";
+import FormTextarea from "./FormTextarea";
+import FormSelect from "./FormSelect";
+import FormRadioGroup from "./FormRadioGroup";
+import FormCheckbox from "./FormCheckbox";
 
-const FormRenderer = <T extends FieldValues>({
-  label,
-  name,
-  control,
-  text,
-  id,
-  type,
-  placeholder,
-  className,
-}: FormRendererProps<T>) => {
+const FormRenderer = <T extends FieldValues>({ FormFields, methods }: FormRendererProps<T>) => {
   return (
-    <FormField
-      control={control}
-      name={name as Path<T>}
-      render={({ field }) => (
-        <FormItem
-          className={cn(type === "checkbox" ? "flex flex-row items-start space-x-3 space-y-0 rounded-md" : null)}
-        >
-          {type === "checkbox" ? null : (
-            <FormLabel className="cursor-pointer" htmlFor={id}>
-              {label}
-            </FormLabel>
-          )}
-          <FormControl>
-            {type === "checkbox" ? (
-              <Checkbox id={id} checked={field.value} onCheckedChange={field.onChange} className={className} />
-            ) : type === "textarea" ? (
-              <Textarea placeholder={placeholder} rows={1} className={cn("resize-none", className)} {...field} />
-            ) : (
-              <Input {...field} id={id} type={type} placeholder={placeholder} className={className} />
-            )}
-          </FormControl>
-          {type === "checkbox" && (
-            <div className="!space-y-1 leading-none">
-              <FormLabel className="cursor-pointer" htmlFor={id}>
-                {label}
-              </FormLabel>
-              <FormDescription>{text}</FormDescription>
-              <FormMessage />
-            </div>
-          )}
-          {type !== "checkbox" && <FormMessage />}
-        </FormItem>
-      )}
-    />
+    <FormProvider {...methods}>
+      <div className="my-5">
+        {FormFields.map((field: FormFieldConfig<T>) =>
+          field.type === "textarea" ? (
+            <FormTextarea
+              key={field.name as string}
+              label={field.label}
+              name={field.name}
+              type="textarea"
+              placeholder={field.placeholder}
+              required={field.required}
+            />
+          ) : field.type === "radio" ? (
+            <FormRadioGroup
+              key={field.name as string}
+              id={field.id}
+              label={field.label}
+              name={field.name}
+              type="radio"
+            />
+          ) : field.type === "select" ? (
+            <FormSelect
+              key={field.name as string}
+              label={field.label}
+              name={field.name}
+              options={field.options}
+              type="select"
+            />
+          ) : field.type === "checkbox" ? (
+            <FormCheckbox key={field.name as string} label={field.label} name={field.name} type="checkbox" />
+          ) : (
+            <FormInput
+              key={field.name as string}
+              label={field.label}
+              name={field.name}
+              type={field.type}
+              placeholder={field.placeholder}
+              required={field.required}
+            />
+          )
+        )}
+      </div>
+    </FormProvider>
   );
 };
 
