0ec448c3aab053b01c4d6d3edb404eb99f2951e2
Jethro <ttppoo12144@gmail.com>
2024-10-27 15:39:18 +1100

    refactor:優化 表單用type

--- src/components/FormRenderer/types.ts
+++ src/components/FormRenderer/types.ts
@@ -1,15 +1,35 @@
-import { Control, FieldValues } from "react-hook-form";
+import { FieldValues, UseFormReturn } from "react-hook-form";
+
+interface Option {
+  value: string;
+  label: string;
+}
 
 export interface FormFieldConfig<T extends FieldValues> {
   id?: string;
   label: string;
+  key: string;
   name: keyof T;
-  type: "text" | "password" | "email" | "number" | "tel" | "checkbox" | "textarea";
+  type:
+    | "text"
+    | "password"
+    | "email"
+    | "number"
+    | "tel"
+    | "checkbox"
+    | "textarea"
+    | "select"
+    | "switch"
+    | "date"
+    | "radio";
   text?: string;
   className?: string;
   placeholder?: string;
+  options?: Option[];
+  required?: boolean;
 }
 
-export interface FormRendererProps<T extends FieldValues> extends FormFieldConfig<T> {
-  control: Control<T>;
+export interface FormRendererProps<T extends FieldValues> {
+  FormFields: FormFieldConfig<T>[];
+  methods: UseFormReturn<T>;
 }
