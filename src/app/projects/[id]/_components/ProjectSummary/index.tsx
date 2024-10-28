4367b083046a7f5866cc4bb429fc9eeb0c3f0b12
mionne <neohyntw@gmail.com>
2024-10-28 17:41:25 +0800

    新增ProjectList

--- src/app/projects/[id]/_components/ProjectSummary/index.tsx
+++ src/app/projects/[id]/_components/ProjectSummary/index.tsx
@@ -16,8 +16,11 @@ import {
   LineShareButton,
   LineIcon,
 } from "next-share";
+import { usePathname } from "next/navigation";
 
 const ProjectSummary = ({ ...ProjectSummaryData }: ProjectSummaryDataProps) => {
+  const pathname = usePathname();
+  const isProjectList = pathname.includes("/projects/") && pathname.includes("/projectlist");
   return (
     <section
       className="container lg:grid lg:gap-0 lg:px-3 lg:pb-[100px] lg:pt-[100px] xl:px-0"
@@ -128,16 +131,18 @@ const ProjectSummary = ({ ...ProjectSummaryData }: ProjectSummaryDataProps) => {
                   </TwitterShareButton>
                 </li>
               </ul>
-              <div className="fixed bottom-0 left-0 right-0 z-10 flex justify-center border-t bg-white p-4 lg:relative lg:justify-start lg:border-t-0 lg:p-0">
-                <Button className="mr-5" variant="donateNow" size="lg" asChild>
-                  <Link scroll={true} href="#">
-                    立即贊助
-                  </Link>
-                </Button>
-                <Button className="flex-shrink-0" variant="secondary" size="icon">
-                  <TiHeartFullOutline size={24} />
-                </Button>
-              </div>
+              {isProjectList ? null : (
+                <div className="fixed bottom-0 left-0 right-0 z-10 flex justify-center border-t bg-white p-4 lg:relative lg:justify-start lg:border-t-0 lg:p-0">
+                  <Button className="mr-5" variant="donateNow" size="lg" asChild>
+                    <Link href={`/projects/${ProjectSummaryData.id}/projectlist`} target="_blank">
+                      立即贊助
+                    </Link>
+                  </Button>
+                  <Button className="flex-shrink-0" variant="secondary" size="icon">
+                    <TiHeartFullOutline size={24} />
+                  </Button>
+                </div>
+              )}
             </div>
           </div>
         </div>
---
+++ src/app/projects/[id]/content.tsx
@@ -0,0 +1,35 @@
+"use client";
+import Navbar from "./@Navbar/page";
+import Rewards from "./@Rewards/page";
+import RewardsBar from "./projectlist/page";
+import SectionPadding from "@/components/SectionPadding";
+import { usePathname } from "next/navigation";
+
+export function LayoutContent({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+  const isProjectList = pathname.includes("/projectlist");
+
+  return (
+    <>
+      {isProjectList ? (
+        <>
+          <SectionPadding className="container px-3 lg:pt-0">
+            <RewardsBar />
+          </SectionPadding>
+        </>
+      ) : (
+        <>
+          <Navbar />
+          <SectionPadding className="container px-3">
+            <div className="lg:grid lg:grid-cols-12 lg:gap-6">
+              <div className="mb-8 pr-10 lg:col-span-9">{children}</div>
+              <Rewards />
+            </div>
+          </SectionPadding>
+        </>
+      )}
+    </>
+  );
+}
+
+export default LayoutContent;
