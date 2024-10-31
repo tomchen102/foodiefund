18ebce0388d4e4b2e4deed5c52880bfc93b031c2
Jethro <ttppoo12144@gmail.com>
2024-10-31 21:17:29 +1100

    feat: 新增 useEffect 鉤子設定預設值和 halfWidth 屬性到 FormSelect 元件 讓布局更彈性和使用者體驗優化

--- src/components/FormRenderer/FormSelect.tsx
+++ src/components/FormRenderer/FormSelect.tsx
@@ -1,19 +1,37 @@
-import { FieldValues, Path, useFormContext } from "react-hook-form";
+import { FieldValues, Path, PathValue, useFormContext } from "react-hook-form";
 import { FormControl, FormField, FormItem, FormLabel, FormMessage } from "../ui/form";
 import { FormFieldConfig } from "./types";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../ui/select";
+import { cn } from "@/lib/utils";
+import { useEffect } from "react";
 
-const FormSelect = <T extends FieldValues>({ label, name, options, placeholder }: FormFieldConfig<T>) => {
-  const { control } = useFormContext<T>();
+const FormSelect = <T extends FieldValues>({ label, name, options, placeholder, halfWidth }: FormFieldConfig<T>) => {
+  const { control, setValue, watch } = useFormContext<T>();
+  const currentValue = watch(name as Path<T>);
+
+  useEffect(() => {
+    if (options && options.length > 0) {
+      const valueExists = options.some((option) => option.value === currentValue);
+
+      if (!valueExists) {
+        setValue(name as Path<T>, options[0].value as PathValue<T, Path<T>>, {
+          shouldValidate: true,
+          shouldDirty: true,
+          shouldTouch: true,
+        });
+      }
+    }
+  }, [options, setValue, name, currentValue]);
+
   return (
-    <div className="my-5">
+    <div className={cn("my-5", halfWidth ? "w-1/2" : "w-full")}>
       <FormField
         control={control}
         name={name as Path<T>}
         render={({ field }) => (
           <FormItem>
             <FormLabel>{label}</FormLabel>
-            <Select onValueChange={field.onChange} value={field.value || ""}>
+            <Select onValueChange={field.onChange} value={field.value} defaultValue={options?.[0]?.value}>
               <FormControl>
                 <SelectTrigger>
                   <SelectValue placeholder={placeholder} />
