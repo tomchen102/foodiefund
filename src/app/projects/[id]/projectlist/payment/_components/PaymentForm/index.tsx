5f8ab42bf77421b52511ac55a1a8570bb1e8a983
Jethro <ttppoo12144@gmail.com>
2024-11-02 01:28:53 +1100

    fix:debug 把不必要的畫面移除

--- src/app/projects/[id]/projectlist/payment/_components/PaymentForm/index.tsx
+++ src/app/projects/[id]/projectlist/payment/_components/PaymentForm/index.tsx
@@ -107,15 +107,6 @@ const PaymentForm = () => {
 
   return (
     <Form {...form}>
-      {areas.length > 0 && cities.length > 0 && (
-        <div className="flex flex-col space-y-6 md:flex-row md:space-x-6 md:space-y-0">
-          <div className="w-full">
-            {areas.map((item) => {
-              return <div key={item.value}>{item.label}</div>;
-            })}
-          </div>
-        </div>
-      )}
       <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-6">
         <FormRenderer<PaymentFormType> methods={form} FormFields={updatedPaymentFormFields} />
         <Button type="submit">立即付款</Button>
