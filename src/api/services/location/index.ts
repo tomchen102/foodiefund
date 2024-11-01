5609f5b771ba68c5adc01417ef45c9845c452117
Jethro <ttppoo12144@gmail.com>
2024-11-02 00:26:57 +1100

    flx:修正 getLocation 線上路徑

--- src/api/services/location/index.ts
+++ src/api/services/location/index.ts
@@ -2,7 +2,7 @@ import axiosClient from "@/api/axiosClient";
 
 export const getLocation = async () => {
   const response = await axiosClient(
-    `${process.env.NODE_ENV === "development" ? "http://localhost:3000" : process.env.NEXT_PUBLIC_BASE_URL}/CityCountyData.json`
+    `${process.env.NODE_ENV === "development" ? "http://localhost:3000" : ""}/CityCountyData.json`
   );
   return response.data;
 };
