2bcaa33b4213d41e08c9c4bb13773e93984bd212
Jethro <ttppoo12144@gmail.com>
2024-11-03 01:50:42 +1100

    style:近期專案 增加高度 讓內容撐開 原本的UI跑版了

--- src/components/RecentProjectsBlock/PcRecentProjectsList.tsx
+++ src/components/RecentProjectsBlock/PcRecentProjectsList.tsx
@@ -19,8 +19,8 @@ const PcRecentProjectsList = ({ data, gridType }: RecentProjectsListProps) => {
         return (
           <li className="relative flex flex-col" key={item.id}>
             <div className="flex h-full flex-col rounded-lg border border-solid border-gray-200 bg-white">
-              <Link scroll={true} href={href} className="group">
-                <div className="flex flex-col">
+              <Link scroll={true} href={href} className="group block h-full">
+                <div className="flex h-full flex-col">
                   <div className="relative">
                     <Image
                       src={item.imageUrl}
