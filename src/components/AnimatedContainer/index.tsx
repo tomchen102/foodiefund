e5e05a7165997a7295ba88349ebb7a6996edcfe9
tom <tomchenforwork@gmail.com>
2024-11-09 18:03:37 +0800

    fix: loading error修改

--- src/components/AnimatedContainer/index.tsx
+++ src/components/AnimatedContainer/index.tsx
@@ -3,14 +3,14 @@
 import { ProviderProps } from "@/types/ProviderType";
 import { motion } from "framer-motion";
 
-export default function AnimatedContainer({ children, className }: ProviderProps) {
+export default function AnimatedContainer({ children, ...props }: ProviderProps) {
   return (
     <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
       transition={{ ease: "easeInOut", duration: 0.3 }}
-      className={className}
+      {...props}
     >
       {children}
     </motion.div>
