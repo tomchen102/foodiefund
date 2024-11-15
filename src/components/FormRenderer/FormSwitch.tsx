b31b8636e231fed390c207350b2ddec0f14ad25f
Jethro <ttppoo12144@gmail.com>
2024-11-15 21:36:03 +1100

    feat:新增 FormSwitch 組件 用於表單switch使用的組件

---
+++ src/components/FormRenderer/FormSwitch.tsx
@@ -0,0 +1,32 @@
+import { FieldValues, Path, useFormContext } from "react-hook-form";
+import { FormControl, FormField, FormItem, FormLabel, FormMessage } from "../ui/form";
+import { FormFieldConfig } from "./types";
+import { Switch } from "../ui/switch";
+
+const FormSwitch = <T extends FieldValues>({ label, name, id }: FormFieldConfig<T>) => {
+  const { control } = useFormContext<T>();
+  return (
+    <div className="my-5">
+      <FormField
+        control={control}
+        name={name as Path<T>}
+        render={({ field }) => (
+          <FormItem className="flex items-center">
+            <FormLabel className="mr-3 mt-2">{label}</FormLabel>
+            <FormControl>
+              <Switch
+                id={id}
+                {...field}
+                checked={field.value || false}
+                onCheckedChange={(checked) => field.onChange(checked)}
+              />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+    </div>
+  );
+};
+
+export default FormSwitch;
