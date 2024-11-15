8ea250d0552adc45a3009d18363f02aa5a8f469b
Jethro <ttppoo12144@gmail.com>
2024-11-15 21:40:02 +1100

    feat:新增 DataTablePagination 組件 用於table的分頁使用

---
+++ src/components/Table/DataTablePagination.tsx
@@ -0,0 +1,89 @@
+import { ChevronLeftIcon, ChevronRightIcon, DoubleArrowLeftIcon, DoubleArrowRightIcon } from "@radix-ui/react-icons";
+import { Table } from "@tanstack/react-table";
+import { Button } from "@/components/ui/button";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+
+interface DataTablePaginationProps<TData> {
+  table: Table<TData>;
+  currentPage: number;
+  pageSize: number;
+  handlePageChange: (page: number, pageSize?: number) => void;
+  totalPages: number;
+}
+
+export function DataTablePagination<TData>({
+  table,
+  currentPage,
+  pageSize,
+  handlePageChange,
+  totalPages,
+}: DataTablePaginationProps<TData>) {
+  const canPreviousPage = currentPage > 0;
+  const canNextPage = currentPage < totalPages - 1;
+
+  return (
+    <div className="flex items-center justify-between px-2">
+      <div className="flex-1 text-sm text-muted-foreground">
+        {table.getFilteredSelectedRowModel().rows.length} of {table.getCoreRowModel().rows.length} row(s) selected.
+      </div>
+      <div className="flex items-center space-x-6 lg:space-x-8">
+        <div className="flex items-center space-x-2">
+          <p className="text-sm font-medium">每頁行數</p>
+          <Select value={`${pageSize}`} onValueChange={(value) => handlePageChange(0, Number(value))}>
+            <SelectTrigger className="h-8 w-[70px]">
+              <SelectValue placeholder={pageSize} />
+            </SelectTrigger>
+            <SelectContent side="top">
+              {[10, 20, 30, 40, 50].map((size) => (
+                <SelectItem key={size} value={`${size}`}>
+                  {size}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        </div>
+        <div className="flex w-[100px] items-center justify-center text-sm font-medium">
+          第 {currentPage + 1} 到 {totalPages} 頁
+        </div>
+        <div className="flex items-center space-x-2">
+          <Button
+            variant="outline"
+            className="hidden h-8 w-8 p-0 lg:flex"
+            onClick={() => handlePageChange(0)}
+            disabled={!canPreviousPage}
+          >
+            <span className="sr-only">转到第一页</span>
+            <DoubleArrowLeftIcon className="h-4 w-4" />
+          </Button>
+          <Button
+            variant="outline"
+            className="h-8 w-8 p-0"
+            onClick={() => handlePageChange(currentPage - 1)}
+            disabled={!canPreviousPage}
+          >
+            <span className="sr-only">转到上一页</span>
+            <ChevronLeftIcon className="h-4 w-4" />
+          </Button>
+          <Button
+            variant="outline"
+            className="h-8 w-8 p-0"
+            onClick={() => handlePageChange(currentPage + 1)}
+            disabled={!canNextPage}
+          >
+            <span className="sr-only">转到下一页</span>
+            <ChevronRightIcon className="h-4 w-4" />
+          </Button>
+          <Button
+            variant="outline"
+            className="hidden h-8 w-8 p-0 lg:flex"
+            onClick={() => handlePageChange(totalPages - 1)}
+            disabled={!canNextPage}
+          >
+            <span className="sr-only">转到最后一页</span>
+            <DoubleArrowRightIcon className="h-4 w-4" />
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
