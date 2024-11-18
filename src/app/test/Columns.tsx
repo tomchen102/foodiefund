2b0e4b780adfeb98923e640f36f4770b164f771d
Jethro <ttppoo12144@gmail.com>
2024-11-18 23:32:36 +1100

    refactor:更改資料型態 和 新增 欄位 新增isTop的欄位

--- src/app/test/Columns.tsx
+++ src/app/test/Columns.tsx
@@ -2,23 +2,32 @@
 import { Pencil1Icon, TrashIcon } from "@radix-ui/react-icons";
 import { ColumnDef } from "@tanstack/react-table";
 import { Button } from "@/components/ui/button";
-import { ITableData } from "./page";
 import { Switch } from "@/components/ui/switch";
+import { UserNewsListResponseType } from "@/api/services/userNews/types";
 
 export const createColumns = (
-  handleEdit: (item: ITableData) => void,
-  handleDelete: (item: ITableData) => void
-): ColumnDef<ITableData>[] => [
+  handleEdit: (item: UserNewsListResponseType) => void,
+  handleDelete: (item: UserNewsListResponseType) => void
+): ColumnDef<UserNewsListResponseType>[] => [
   {
-    accessorKey: "id",
+    id: "index",
     header: "編號",
+    cell: ({ row }) => row.index + 1,
   },
   {
     accessorKey: "isActive",
-    header: "是否開啟",
+    header: "是否啟用",
     cell: ({ row }) => {
       const item = row?.original;
       return <Switch checked={item.isActive}></Switch>;
+    },
+  },
+  {
+    accessorKey: "isTop",
+    header: "是否置頂",
+    cell: ({ row }) => {
+      const item = row?.original;
+      return <Switch checked={item.isTop}></Switch>;
     },
   },
   {
@@ -26,7 +35,9 @@ export const createColumns = (
     header: "發表時間",
     cell: ({ row }) => {
       const date = new Date(row.getValue("publicAt"));
-      return <div className="lowercase">{date.toLocaleDateString()}</div>;
+      return (
+        <div className="lowercase">{date.toLocaleDateString() === "Invalid Date" ? "" : date.toLocaleDateString()}</div>
+      );
     },
   },
   {
