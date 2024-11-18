f06258a74a32c9f38ad1deba017a676dbce8943e
Jethro <ttppoo12144@gmail.com>
2024-11-18 23:25:31 +1100

    chore:reactCompiler會有表單驗證出不瞭問題 先改掉

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    reactCompiler: true,
+    reactCompiler: {
+      compilationMode: "annotation",
+    },
   },
   images: {
     remotePatterns: [
