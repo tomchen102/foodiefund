4d0f5cdca0aa41adb69cd58829423ab71823bf3b
Jethro <ttppoo12144@gmail.com>
2024-11-23 16:46:17 +1100

    feat:新增 MSW userNews的API數據

---
+++ src/lib/msw/handlers/userNewsHandlers/data.ts
@@ -0,0 +1,64 @@
+import { UserNewsListResponseType } from "@/api/services/userNews/types";
+
+export const allUserNews: UserNewsListResponseType[] = [
+  {
+    id: "1",
+    title: "我選錯方案了，請問可以修改贊助回饋項目嗎？",
+    content: "我選錯方案了 請問可以修改贊助回饋項目嗎？",
+    isActive: true,
+    isTop: true,
+    publicAt: "2024-11-11",
+    image:
+      "https://storage.googleapis.com/fir-express-80358.appspot.com/images/d60185ce-4b9f-4fcf-9957-dc0388b11364.jpg?GoogleAccessId=firebase-adminsdk-42otj%40fir-express-80358.iam.gserviceaccount.com&Expires=16756675200&Signature=iWsH8%2B7bVrKBGa2FW4Wc%2B8UYcqvS4b4%2BH5nwKg7COO29%2FDCCbGQeMggFy486SCX2Jp%2F9%2BK%2FCxS4TkRqTYijrvai60R7n%2Fy7iwEeq4qvWTXHempJrIPRK0K%2FtJSxR33sMRtfomyGhfwk1qpQnO9VYdgdpipr6jaQO7lij0I8Bw5pN4AaJzPfvi8aIPMYW%2Fq%2Bp8903JvF99aFIYTLeOSyTg70XTyhdoeQqnE6cEy%2BWeTgdhwT0WSybLhqG0yW%2BsZ56mCe1Q7ekp1j3xl1gXHEI%2B%2Bx%2BbUzo2d3fi7ewDQYHZSPsgjPoK3b4Hn3OdB1ogOynxUIJbGhXuP%2F07p2S8LhkyA%3D%3D",
+  },
+  {
+    id: "2",
+    title: "想要購買的數量較多，團購還會有優惠折扣嗎？",
+    content: "我選錯方案了 請問可以修改贊助回饋項目嗎？",
+    isActive: false,
+    isTop: true,
+    publicAt: "2024-11-11",
+    image:
+      "https://storage.googleapis.com/fir-express-80358.appspot.com/images/d60185ce-4b9f-4fcf-9957-dc0388b11364.jpg?GoogleAccessId=firebase-adminsdk-42otj%40fir-express-80358.iam.gserviceaccount.com&Expires=16756675200&Signature=iWsH8%2B7bVrKBGa2FW4Wc%2B8UYcqvS4b4%2BH5nwKg7COO29%2FDCCbGQeMggFy486SCX2Jp%2F9%2BK%2FCxS4TkRqTYijrvai60R7n%2Fy7iwEeq4qvWTXHempJrIPRK0K%2FtJSxR33sMRtfomyGhfwk1qpQnO9VYdgdpipr6jaQO7lij0I8Bw5pN4AaJzPfvi8aIPMYW%2Fq%2Bp8903JvF99aFIYTLeOSyTg70XTyhdoeQqnE6cEy%2BWeTgdhwT0WSybLhqG0yW%2BsZ56mCe1Q7ekp1j3xl1gXHEI%2B%2Bx%2BbUzo2d3fi7ewDQYHZSPsgjPoK3b4Hn3OdB1ogOynxUIJbGhXuP%2F07p2S8LhkyA%3D%3D",
+  },
+  {
+    id: "3",
+    title: "Unpaid",
+    content: "我選錯方案了 請問可以修改贊助回饋項目嗎？",
+    isActive: true,
+    isTop: true,
+    publicAt: "2024-11-11",
+    image:
+      "https://storage.googleapis.com/fir-express-80358.appspot.com/images/d60185ce-4b9f-4fcf-9957-dc0388b11364.jpg?GoogleAccessId=firebase-adminsdk-42otj%40fir-express-80358.iam.gserviceaccount.com&Expires=16756675200&Signature=iWsH8%2B7bVrKBGa2FW4Wc%2B8UYcqvS4b4%2BH5nwKg7COO29%2FDCCbGQeMggFy486SCX2Jp%2F9%2BK%2FCxS4TkRqTYijrvai60R7n%2Fy7iwEeq4qvWTXHempJrIPRK0K%2FtJSxR33sMRtfomyGhfwk1qpQnO9VYdgdpipr6jaQO7lij0I8Bw5pN4AaJzPfvi8aIPMYW%2Fq%2Bp8903JvF99aFIYTLeOSyTg70XTyhdoeQqnE6cEy%2BWeTgdhwT0WSybLhqG0yW%2BsZ56mCe1Q7ekp1j3xl1gXHEI%2B%2Bx%2BbUzo2d3fi7ewDQYHZSPsgjPoK3b4Hn3OdB1ogOynxUIJbGhXuP%2F07p2S8LhkyA%3D%3D",
+  },
+  {
+    id: "4",
+    title: "Paid",
+    content: "我選錯方案了 請問可以修改贊助回饋項目嗎？",
+    isActive: false,
+    isTop: true,
+    publicAt: "2024-11-11",
+    image:
+      "https://storage.googleapis.com/fir-express-80358.appspot.com/images/d60185ce-4b9f-4fcf-9957-dc0388b11364.jpg?GoogleAccessId=firebase-adminsdk-42otj%40fir-express-80358.iam.gserviceaccount.com&Expires=16756675200&Signature=iWsH8%2B7bVrKBGa2FW4Wc%2B8UYcqvS4b4%2BH5nwKg7COO29%2FDCCbGQeMggFy486SCX2Jp%2F9%2BK%2FCxS4TkRqTYijrvai60R7n%2Fy7iwEeq4qvWTXHempJrIPRK0K%2FtJSxR33sMRtfomyGhfwk1qpQnO9VYdgdpipr6jaQO7lij0I8Bw5pN4AaJzPfvi8aIPMYW%2Fq%2Bp8903JvF99aFIYTLeOSyTg70XTyhdoeQqnE6cEy%2BWeTgdhwT0WSybLhqG0yW%2BsZ56mCe1Q7ekp1j3xl1gXHEI%2B%2Bx%2BbUzo2d3fi7ewDQYHZSPsgjPoK3b4Hn3OdB1ogOynxUIJbGhXuP%2F07p2S8LhkyA%3D%3D",
+  },
+  {
+    id: "5",
+    title: "Paid",
+    content: "我選錯方案了 請問可以修改贊助回饋項目嗎？",
+    isActive: true,
+    isTop: true,
+    publicAt: "2024-11-11",
+    image:
+      "https://storage.googleapis.com/fir-express-80358.appspot.com/images/d60185ce-4b9f-4fcf-9957-dc0388b11364.jpg?GoogleAccessId=firebase-adminsdk-42otj%40fir-express-80358.iam.gserviceaccount.com&Expires=16756675200&Signature=iWsH8%2B7bVrKBGa2FW4Wc%2B8UYcqvS4b4%2BH5nwKg7COO29%2FDCCbGQeMggFy486SCX2Jp%2F9%2BK%2FCxS4TkRqTYijrvai60R7n%2Fy7iwEeq4qvWTXHempJrIPRK0K%2FtJSxR33sMRtfomyGhfwk1qpQnO9VYdgdpipr6jaQO7lij0I8Bw5pN4AaJzPfvi8aIPMYW%2Fq%2Bp8903JvF99aFIYTLeOSyTg70XTyhdoeQqnE6cEy%2BWeTgdhwT0WSybLhqG0yW%2BsZ56mCe1Q7ekp1j3xl1gXHEI%2B%2Bx%2BbUzo2d3fi7ewDQYHZSPsgjPoK3b4Hn3OdB1ogOynxUIJbGhXuP%2F07p2S8LhkyA%3D%3D",
+  },
+  {
+    id: "6",
+    title: "Pending",
+    content: "我選錯方案了 請問可以修改贊助回饋項目嗎？",
+    isActive: false,
+    isTop: true,
+    publicAt: "2024-11-11",
+    image:
+      "https://storage.googleapis.com/fir-express-80358.appspot.com/images/d60185ce-4b9f-4fcf-9957-dc0388b11364.jpg?GoogleAccessId=firebase-adminsdk-42otj%40fir-express-80358.iam.gserviceaccount.com&Expires=16756675200&Signature=iWsH8%2B7bVrKBGa2FW4Wc%2B8UYcqvS4b4%2BH5nwKg7COO29%2FDCCbGQeMggFy486SCX2Jp%2F9%2BK%2FCxS4TkRqTYijrvai60R7n%2Fy7iwEeq4qvWTXHempJrIPRK0K%2FtJSxR33sMRtfomyGhfwk1qpQnO9VYdgdpipr6jaQO7lij0I8Bw5pN4AaJzPfvi8aIPMYW%2Fq%2Bp8903JvF99aFIYTLeOSyTg70XTyhdoeQqnE6cEy%2BWeTgdhwT0WSybLhqG0yW%2BsZ56mCe1Q7ekp1j3xl1gXHEI%2B%2Bx%2BbUzo2d3fi7ewDQYHZSPsgjPoK3b4Hn3OdB1ogOynxUIJbGhXuP%2F07p2S8LhkyA%3D%3D",
+  },
+];
