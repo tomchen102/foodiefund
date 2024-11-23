d3f8428490c65dc0a936737487c766ee73acb4e0
Jethro <ttppoo12144@gmail.com>
2024-11-23 16:56:32 +1100

    feat:新增 Toaster 提示 新增 修改 刪除 都有得到 Toaster的提示

--- src/hooks/uesUserNews.ts
+++ src/hooks/uesUserNews.ts
@@ -1,62 +1,128 @@
 import { deleteUserNews, getUserNewsList, postUserNews, updateUserNews } from "@/api/services/userNews";
+import { UserNewsListQueryResponseType, UserNewsListResponseType } from "@/api/services/userNews/types";
 import { UserNewsListArrayResponse } from "@/schema/UserNewsSchema";
 import { safeParseResponse } from "@/utils/zodUtils";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { AxiosError } from "axios";
+import { useToast } from "./use-toast";
 
 const userNewsKeys = {
   key: ["UserNews"] as const,
 };
-export const useGetUserNews = () => {
+export const useGetUserNews = (options = {}) => {
   return useQuery({
     queryKey: userNewsKeys.key,
     queryFn: async () => {
       const response = await getUserNewsList();
-      console.log("getUserNewsList response:", response);
-      const result = safeParseResponse(UserNewsListArrayResponse, response);
-      console.log("getUserNewsList response:", result);
+      console.log("getUserNewsList response:", response.data);
+      const result = safeParseResponse(UserNewsListArrayResponse, response.data);
       return result;
     },
+    ...options,
   });
 };
 
 export const usePostUserNewsMutation = () => {
   const queryClient = useQueryClient();
+  const { toast } = useToast();
   return useMutation({
     mutationFn: postUserNews,
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: userNewsKeys.key });
-      console.log("created successfully");
+      toast({
+        description: "新增成功!",
+      });
     },
     onError: (error: AxiosError<AxiosError>) => {
       console.error("Error creating News:", error.response?.data.message);
+      toast({
+        variant: "destructive",
+        description: "新增失敗!",
+      });
     },
   });
 };
 
 export const useUpdateUserNewsMutation = () => {
   const queryClient = useQueryClient();
+  const { toast } = useToast();
   return useMutation({
     mutationFn: updateUserNews,
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: userNewsKeys.key });
-      console.log("User successfully");
+      toast({
+        description: "修改成功!",
+      });
     },
     onError: (error: AxiosError<AxiosError>) => {
       console.error("Error creating News:", error.response?.data.message);
+      toast({
+        variant: "destructive",
+        description: "修改失敗!",
+      });
     },
   });
 };
 
 export const useDeleteUserNewsMutation = () => {
   const queryClient = useQueryClient();
+  const { toast } = useToast();
   return useMutation({
     mutationFn: deleteUserNews,
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: userNewsKeys.key });
+      toast({
+        description: "刪除成功!",
+      });
     },
     onError: (error: AxiosError<AxiosError>) => {
       console.error("Error creating News:", error.response?.data.message);
+      toast({
+        variant: "destructive",
+        description: "刪除失敗!",
+      });
+    },
+  });
+};
+
+export const useUpdateNewsTableMutation = () => {
+  const queryClient = useQueryClient();
+  const { toast } = useToast();
+  return useMutation({
+    mutationFn: updateUserNews,
+    onMutate: async (updatedData: UserNewsListResponseType) => {
+      await queryClient.cancelQueries({ queryKey: userNewsKeys.key });
+      const previousData = queryClient.getQueryData<UserNewsListResponseType[]>(userNewsKeys.key);
+
+      if (!previousData) {
+        console.warn("No data found in cache for key:", userNewsKeys.key);
+        return { previousData: null };
+      }
+      queryClient.setQueryData<UserNewsListQueryResponseType>(userNewsKeys.key, (old) => {
+        if (!old || !old.data) return old;
+        return {
+          ...old,
+          data: old.data.map((item) => (item.id === updatedData.id ? { ...item, ...updatedData } : item)),
+        };
+      });
+      return { previousData };
+    },
+    onError: (error, updatedData, context) => {
+      if (context?.previousData) {
+        queryClient.setQueryData(userNewsKeys.key, context.previousData);
+      }
+      console.log(error, updatedData);
+      toast({
+        variant: "destructive",
+        description: "修改失敗!",
+      });
+    },
+    onSuccess: () => {
+      console.log("Update News Table Success");
+      queryClient.invalidateQueries({ queryKey: userNewsKeys.key });
+      toast({
+        description: "修改成功!",
+      });
     },
   });
 };
