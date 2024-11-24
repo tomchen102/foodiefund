3090dec3939f3d6ee5b33b5b0d46005004fff705
Jethro <ttppoo12144@gmail.com>
2024-11-24 14:32:38 +1100

    feat:新增 提案者的QA tanstack的API 讓開發者直接使用API

---
+++ src/hooks/UserQuestionsAndAnswers.ts
@@ -0,0 +1,90 @@
+import { safeParseResponse } from "@/utils/zodUtils";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { AxiosError } from "axios";
+import { useToast } from "./use-toast";
+import {
+  deleteUserQuestionsAndAnswers,
+  getUserQuestionsAndAnswersList,
+  postUserQuestionsAndAnswers,
+  updateUserQuestionsAndAnswers,
+} from "@/api/services/userQuestionsAndAnswers";
+import { UserQuestionsAndAnswersListArrayResponse } from "@/schema/userQuestionsAndAnswers";
+
+const UserQuestionsAndAnswersKeys = {
+  key: ["UserQuestionsAndAnswers"] as const,
+};
+export const useGetUserQuestionsAndAnswers = (options = {}) => {
+  return useQuery({
+    queryKey: UserQuestionsAndAnswersKeys.key,
+    queryFn: async () => {
+      const response = await getUserQuestionsAndAnswersList();
+      console.log("getUserQuestionsAndAnswersList response:", response.data);
+      const result = safeParseResponse(UserQuestionsAndAnswersListArrayResponse, response.data);
+      return result;
+    },
+    ...options,
+  });
+};
+
+export const usePostUserQuestionsAndAnswersMutation = () => {
+  const queryClient = useQueryClient();
+  const { toast } = useToast();
+  return useMutation({
+    mutationFn: postUserQuestionsAndAnswers,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: UserQuestionsAndAnswersKeys.key });
+      toast({
+        description: "新增成功!",
+      });
+    },
+    onError: (error: AxiosError<AxiosError>) => {
+      console.error("Error creating News:", error.response?.data.message);
+      toast({
+        variant: "destructive",
+        description: "新增失敗!",
+      });
+    },
+  });
+};
+
+export const useUpdateUserQuestionsAndAnswersMutation = () => {
+  const queryClient = useQueryClient();
+  const { toast } = useToast();
+  return useMutation({
+    mutationFn: updateUserQuestionsAndAnswers,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: UserQuestionsAndAnswersKeys.key });
+      toast({
+        description: "修改成功!",
+      });
+    },
+    onError: (error: AxiosError<AxiosError>) => {
+      console.error("Error creating News:", error.response?.data.message);
+      toast({
+        variant: "destructive",
+        description: "修改失敗!",
+      });
+    },
+  });
+};
+
+export const useDeleteUserQuestionsAndAnswersMutation = () => {
+  const queryClient = useQueryClient();
+  const { toast } = useToast();
+  return useMutation({
+    mutationFn: deleteUserQuestionsAndAnswers,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: UserQuestionsAndAnswersKeys.key });
+      toast({
+        description: "刪除成功!",
+      });
+    },
+    onError: (error: AxiosError<AxiosError>) => {
+      console.error("Error creating News:", error.response?.data.message);
+      toast({
+        variant: "destructive",
+        description: "刪除失敗!",
+      });
+    },
+  });
+};
