2b3e7da0fd3c57e7c68e7f262768cc0c6b16d562
Jethro <ttppoo12144@gmail.com>
2024-11-24 14:23:40 +1100

    refactor移除不必要的型別

--- src/api/services/signFlow/index.ts
+++ src/api/services/signFlow/index.ts
@@ -1,14 +1,13 @@
 import axiosClient from "@/api/axiosClient";
 import { FormLoginSchemaType, FormRegisterSchemaType } from "@/schema/UserAuth";
-import { AxiosResponse } from "axios";
 import { DataResponse } from "./types";
 
-export const signUp = async (user: FormRegisterSchemaType): Promise<AxiosResponse<DataResponse>> => {
+export const signUp = async (user: FormRegisterSchemaType) => {
   const response = await axiosClient.post<DataResponse>("/auth/sign_up", user);
   return response;
 };
 
-export const signIn = async (user: FormLoginSchemaType): Promise<AxiosResponse<DataResponse>> => {
+export const signIn = async (user: FormLoginSchemaType) => {
   const response = await axiosClient.post<DataResponse>("/auth/sign_in", user);
   return response;
 };
