c8a1ee641173b50cda31565ead53ccc35b9aaa85
Jethro <ttppoo12144@gmail.com>
2024-12-08 14:59:42 +1100

    refactor:移除不必要的泛型

--- src/api/services/news/index.ts
+++ src/api/services/news/index.ts
@@ -1,8 +1,8 @@
 import axiosClient from "@/api/axiosClient";
-import { NewsQueryParams, NewsResponseType } from "./types";
+import { NewsQueryParams } from "./types";
 
 export const getNews = ({ keyWord = "", page, limit = 10 }: NewsQueryParams) => {
-  return axiosClient.get<NewsResponseType>("/news", {
+  return axiosClient.get("/news", {
     params: { keyWord, page, limit },
   });
 };
