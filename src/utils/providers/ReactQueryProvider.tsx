e8f2cb3dd6937d008c83f6d6b08aefa06da7cbd8
Jethro <ttppoo12144@gmail.com>
2024-12-08 15:12:40 +1100

    feat:新增 全局 API 錯誤會反映到toast讓使用者知道錯誤

--- src/utils/providers/ReactQueryProvider.tsx
+++ src/utils/providers/ReactQueryProvider.tsx
@@ -1,11 +1,31 @@
 "use client";
 
+import { useToast } from "@/hooks/use-toast";
 import { ProviderProps } from "@/types/ProviderType";
-import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { QueryClientProvider, QueryClient, QueryCache } from "@tanstack/react-query";
 import { useState } from "react";
 
 const ReactQueryProvider = ({ children }: ProviderProps) => {
-  const [queryClient] = useState(() => new QueryClient());
+  const { toast } = useToast();
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            retry: 1,
+          },
+        },
+        queryCache: new QueryCache({
+          onError: (error) => {
+            console.error("Global Query Error:", error);
+            return toast({
+              variant: "destructive",
+              description: `${error}`,
+            });
+          },
+        }),
+      })
+  );
 
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 };
