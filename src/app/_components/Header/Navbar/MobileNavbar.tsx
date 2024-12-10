c6af3b673e2d496b8959040f21ba6b86ea52c43e
chindesu0207 <potter0207@gmail.com>
2024-12-10 23:26:05 +0800

    feat: 新增 Navbar 及 MobileNavbar 元件

---
+++ src/app/_components/Header/Navbar/MobileNavbar.tsx
@@ -0,0 +1,137 @@
+"use client";
+import { useState } from "react";
+import Link from "next/link";
+import Logo from "../../Logo";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+  SheetSubContent,
+} from "@/components/ui/sheet";
+import { FaDollarSign, FaRegHeart, FaRegUser } from "react-icons/fa";
+import { ChevronRightIcon, ChevronLeftIcon, ExitIcon, HamburgerMenuIcon, Pencil1Icon } from "@radix-ui/react-icons";
+import AvatarCircle from "../AvatarCircle";
+import { useAuth } from "@/utils/providers/AuthProvider";
+import { MenuProps, MemberMenuType } from "../types";
+
+const memberMenu: MemberMenuType = [
+  { title: "帳號設定", href: "#", icon: FaRegUser },
+  { title: "贊助紀錄", href: "#", icon: FaDollarSign },
+  { title: "收藏紀錄", href: "#", icon: FaRegHeart },
+  { title: "提案紀錄", href: "#", icon: Pencil1Icon },
+];
+
+const MobileNavbar = ({ menuList }: MenuProps) => {
+  const { user, clearUser } = useAuth();
+  const handleLogout = () => clearUser();
+  const [isAllSheetsOpen, setIsAllSheetsOpen] = useState<boolean>(false);
+  const [isChildSheetOpen, setIsChildSheetOpen] = useState<boolean>(false);
+
+  const closeAllSheets = () => {
+    setIsAllSheetsOpen(false);
+    setIsChildSheetOpen(false);
+  };
+
+  return (
+    <Sheet open={isAllSheetsOpen} onOpenChange={setIsAllSheetsOpen}>
+      <SheetTrigger className="flex h-12 w-12 items-center justify-center md:hidden">
+        <HamburgerMenuIcon className="h-6 w-6 text-primary-dark" />
+      </SheetTrigger>
+      <SheetContent className="h-full w-full p-0">
+        <SheetHeader className="flex h-12 items-center justify-center pl-3">
+          <Logo />
+          <SheetTitle className="hidden"></SheetTitle>
+          <SheetDescription className="hidden"></SheetDescription>
+        </SheetHeader>
+        {user && (
+          <Sheet open={isChildSheetOpen} onOpenChange={setIsChildSheetOpen}>
+            <SheetTrigger className="flex h-11 w-full items-center bg-primary pl-3 hover:bg-primary-light">
+              <AvatarCircle />
+              <div className="w-16 truncate text-base font-bold capitalize text-primary-dark">{user.name}</div>
+              <div className="absolute right-0 flex size-11 items-center justify-center">
+                <ChevronRightIcon className="size-5 text-primary-dark" />
+              </div>
+            </SheetTrigger>
+            <SheetSubContent className="absolute top-12 size-full p-0">
+              <SheetTitle className="hidden"></SheetTitle>
+              <SheetDescription className="hidden"></SheetDescription>
+              <Link
+                href="/"
+                className="absolute -top-12 left-0 h-12 w-28 bg-transparent"
+                onClick={closeAllSheets}
+              ></Link>
+              <button className="absolute -top-12 right-0 size-12 bg-transparent" onClick={closeAllSheets}></button>
+              <SheetClose className="flex w-full items-center bg-primary text-base font-bold text-primary-dark hover:bg-primary-light">
+                <div className="flex size-11 items-center justify-center">
+                  <ChevronLeftIcon className="size-5 text-primary-dark" />
+                </div>
+                返回
+              </SheetClose>
+              {memberMenu.map((item) => {
+                return (
+                  <div
+                    className="w-full border-b border-gray-200 last:border-b-0"
+                    key={item.title}
+                    onClick={closeAllSheets}
+                  >
+                    <Link
+                      className="flex h-12 min-h-[3.25rem] items-center gap-2 pl-3 text-base font-bold text-primary-dark"
+                      href={item.href}
+                      key={item.title}
+                    >
+                      <div className="mr-2 flex size-5 items-center justify-center">
+                        {<item.icon className="size-4" />}
+                      </div>
+                      {item.title}
+                    </Link>
+                  </div>
+                );
+              })}
+              <div className="w-full" onClick={closeAllSheets}>
+                <div
+                  className="flex h-11 w-full items-center justify-center bg-primary text-base font-bold text-primary-dark hover:bg-primary-light"
+                  onClick={handleLogout}
+                >
+                  <div className="mr-2 flex size-5 items-center justify-center">
+                    <ExitIcon className="size-4 text-primary-dark" />
+                  </div>
+                  登出
+                </div>
+              </div>
+            </SheetSubContent>
+          </Sheet>
+        )}
+        {menuList.map((item) => (
+          <SheetClose className="w-full border-b border-gray-200 last:border-b-0" key={item.title} asChild>
+            <Link
+              key={item.title}
+              className="flex min-h-[3.25rem] w-full items-center pl-3 font-bold text-primary-dark"
+              href={item.href}
+            >
+              {item.title}
+            </Link>
+          </SheetClose>
+        ))}
+        {!user && (
+          <SheetFooter>
+            <SheetClose asChild>
+              <Link
+                href="/login"
+                className="flex h-11 w-full cursor-pointer items-center justify-center bg-primary text-base font-bold text-primary-dark hover:bg-primary-light"
+              >
+                登錄 / 註冊
+              </Link>
+            </SheetClose>
+          </SheetFooter>
+        )}
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default MobileNavbar;
---
+++ src/app/_components/Header/Navbar/index.tsx
@@ -0,0 +1,29 @@
+"use client";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { MenuProps } from "../types";
+
+const Navbar = ({ menuList }: MenuProps) => {
+  return (
+    <nav className="hidden h-full md:block">
+      <ul className="z-10 flex h-full gap-8 space-x-5">
+        {menuList.map((item) => (
+          <li className="group relative" key={item.title}>
+            <Link
+              href={item.href}
+              className={cn(
+                "flex h-full w-fit items-center justify-center px-1 text-base font-bold text-primary-dark",
+                {}
+              )}
+            >
+              {item.title}
+              <span className="absolute bottom-3 left-0 h-[2px] w-full bg-transparent group-hover:bg-secondary"></span>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
