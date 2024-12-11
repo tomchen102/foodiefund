90158082a31510932c420438e8b491c10c19e446
Jethro <ttppoo12144@gmail.com>
2024-12-11 23:22:51 +1100

    feat:修改mock 使用真實api 目前只供某個專案,之後再修正動態

--- src/api/services/userNews/index.ts
+++ src/api/services/userNews/index.ts
@@ -1,23 +1,38 @@
-import axios from "axios";
 import { UserNewsListResponseType } from "./types";
-
-const baseUrl = process.env.NODE_ENV === "development" ? "userNews" : "userNews";
+import axiosClient from "@/api/axiosClient";
 
 export const getUserNewsList = async () => {
-  const response = await axios.get(`${baseUrl}`);
+  const response = await axiosClient.get(`/plan/64c5ae5c6f2d3e001ccf9abc/news`);
+  console.log("response", response);
   return response.data;
 };
 
 export const postUserNews = async (data: UserNewsListResponseType) => {
-  const response = await axios.post(`${baseUrl}`, data);
+  const formData = new FormData();
+  if (data.image) {
+    formData.append("file", data.image);
+  }
+  const response = await axiosClient.post(`/plan/64c5ae5c6f2d3e001ccf9abc/news`, data, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
   return response.data;
 };
 
 export const updateUserNews = async (data: UserNewsListResponseType) => {
-  const response = await axios.put(`${baseUrl}/${data.id}`, data);
+  const formData = new FormData();
+  if (data.image) {
+    formData.append("file", data.image);
+  }
+  const response = await axiosClient.put(`/plan/64c5ae5c6f2d3e001ccf9abc/news/${data.id}`, data, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
   return response.data;
 };
 
 export const deleteUserNews = async (id: string) => {
-  return axios.delete(`${baseUrl}/${id}`);
+  return axiosClient.delete(`/plan/64c5ae5c6f2d3e001ccf9abc/news/${id}`);
 };
