67e58c74eac9567e5594e97d713f0903fc63378f
tom <tomchenforwork@gmail.com>
2024-12-22 19:31:44 +0800

    feat: 計畫回饋內容

--- src/app/dashboard/[plan_id]/(tab)/rewards/page.tsx
+++ src/app/dashboard/[plan_id]/(tab)/rewards/page.tsx
@@ -1,20 +1,17 @@
-import SectionPadding from "@/components/SectionPadding";
 import { Button } from "@/components/ui/button";
 import { FiPlus } from "react-icons/fi";
 
 const Rewards = () => {
   return (
-    <SectionPadding className="container px-3 xl:px-0">
-      <div>
-        <div className="mb-5 flex items-center">
-          <h1 className="mr-3">計畫回饋</h1>
-          <Button className="ml-auto" variant="donateNow">
-            <FiPlus size={20} className="mr-2" />
-            新增資料
-          </Button>
-        </div>
+    <div>
+      <div className="my-5 flex items-center">
+        <h1 className="mr-3">計畫回饋</h1>
+        <Button className="ml-auto" variant="donateNow">
+          <FiPlus size={20} className="mr-2" />
+          新增資料
+        </Button>
       </div>
-    </SectionPadding>
+    </div>
   );
 };
 
