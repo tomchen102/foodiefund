6dd094f7164b8e53ca5512e10c86f10cd7312522
tom <tomchenforwork@gmail.com>
2024-12-22 12:56:35 +0800

    feat: 側邊選單select

--- src/app/dashboard/[plan_id]/layout.tsx
+++ src/app/dashboard/[plan_id]/layout.tsx
@@ -1,13 +1,8 @@
 import { ProviderProps } from "@/types/ProviderType";
-import { ChevronDownIcon } from "@radix-ui/react-icons";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import Link from "next/link";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+
 import LayoutContent from "./content";
+import SectionPadding from "@/components/SectionPadding";
 
 const navMenu = {
   projectNav: [
@@ -16,37 +11,31 @@ const navMenu = {
     { title: "龍蝦盛宴", hrefUrl: "#" },
   ],
 };
-
 export default async function ProjectsLayout(props: ProviderProps) {
   const { children } = props;
 
   return (
     <>
-      <div className="container">
+      <SectionPadding className="container px-3 xl:px-0">
         <div className="lg:grid lg:grid-cols-12 lg:gap-6">
           <div className="lg:col-span-3"></div>
-          <div className="lg:pt-10">
-            <DropdownMenu>
-              <DropdownMenuTrigger className="mr-4 flex w-full items-center justify-start gap-2 border border-solid border-gray-200 bg-[#fff] px-3 py-3 focus-visible:outline-none md:min-w-60">
-                <span className="mr-auto text-sm md:ml-1">金華火腿主題咖啡屋</span>
-                <ChevronDownIcon />
-              </DropdownMenuTrigger>
-              <DropdownMenuContent>
-                {navMenu.projectNav.map((item, index) => {
-                  return (
-                    <Link href={item.hrefUrl} key={index}>
-                      <DropdownMenuItem className="border border-solid border-gray-200 hover:!bg-primary-light md:min-w-60">
-                        {item.title}
-                      </DropdownMenuItem>
-                    </Link>
-                  );
-                })}
-              </DropdownMenuContent>
-            </DropdownMenu>
+          <div className="mb-5">
+            <Select>
+              <SelectTrigger className="w-[255px]">
+                <SelectValue placeholder="金華火腿主題咖啡屋" />
+              </SelectTrigger>
+              <SelectContent>
+                {navMenu.projectNav.map((item, index) => (
+                  <SelectItem key={index} value={item.title}>
+                    {item.title}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
           </div>
         </div>
         <LayoutContent>{children}</LayoutContent>
-      </div>
+      </SectionPadding>
     </>
   );
 }
