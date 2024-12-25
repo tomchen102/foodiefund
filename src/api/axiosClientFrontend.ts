da7f71fcf985d90882e03d6a6c8120af8039425c
Jethro <ttppoo12144@gmail.com>
2024-12-25 16:02:41 +1100

    feat:新增 前後台Api baseURL 的實體 並且刪除 axiosClient

---
+++ src/api/axiosClientFrontend.ts
@@ -0,0 +1,24 @@
+"use client";
+import axios from "axios";
+import cookies from "js-cookie";
+import { applyInterceptors } from "./applyInterceptors";
+
+const userCookie = cookies.get("token");
+
+let token = "";
+if (userCookie) {
+  const user = JSON.parse(userCookie);
+  token = user.token;
+}
+
+export const axiosClientFrontend = applyInterceptors(
+  axios.create({
+    baseURL: process.env.NEXT_PUBLIC_FRONTEND_API_URL,
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  })
+);
+
+export default axiosClientFrontend;
