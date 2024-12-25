911d7fdac695894c31966cd5183a5735ae6cc7e4
Jethro <ttppoo12144@gmail.com>
2024-12-25 16:00:48 +1100

    feat:新增applyInterceptors檔案 可以和前台後台API一起共用 interceptors

---
+++ src/api/applyInterceptors.ts
@@ -0,0 +1,23 @@
+import { ErrorResponse } from "@/types/errorResponse";
+import { AxiosError, AxiosInstance, AxiosResponse } from "axios";
+
+export const applyInterceptors = (instance: AxiosInstance): AxiosInstance => {
+  instance.interceptors.response.use(
+    (response: AxiosResponse) => {
+      if (response.data && response.data.data) {
+        response.data = response.data.data;
+      }
+      return response;
+    },
+    (error: AxiosError<ErrorResponse>) => {
+      const status = error?.response?.status || null;
+      if (status === 401) {
+        if (typeof window !== "undefined") {
+          window.location.href = "/login";
+        }
+      }
+      return Promise.reject(error.response?.data);
+    }
+  );
+  return instance;
+};
