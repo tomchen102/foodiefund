5c772df0e44e91c55a2d8e92b337f8c4ed06043b
Jethro <ttppoo12144@gmail.com>
2024-12-25 23:39:22 +1100

    refactor:新增動態引入 NewsContent 元件 和 Suspense 更好的使用者體驗

--- src/app/dashboard/[plan_id]/(tab)/news/page.tsx
+++ src/app/dashboard/[plan_id]/(tab)/news/page.tsx
@@ -1,70 +1,30 @@
 "use client";
 import SectionPadding from "@/components/SectionPadding";
 import { buttonVariants } from "@/components/ui/button";
-import { FiPlus } from "react-icons/fi";
-import { useDeleteUserNewsMutation, useGetUserNews } from "@/hooks/uesUserNews";
-import { Pencil1Icon } from "@radix-ui/react-icons";
-import { Checkbox } from "@/components/ui/checkbox";
 import Link from "next/link";
-import { format } from "date-fns";
-import { AlertDialogTriggerDelete } from "@/components/DeleteDialog/AlertDialogTriggerDelete";
 import { usePathname } from "next/navigation";
-import { cn } from "@/lib/utils";
+import { Icons } from "@/components/Icons";
+import { Suspense, lazy } from "react";
+import Loading from "./loading";
+
+const NewsContent = lazy(() => import("./NewsContent"));
 
 const UserNewsPage = () => {
   const createUserNewsUrl = usePathname();
-
-  const { data } = useGetUserNews();
-  const { mutate: DeleteUserNewsMutation } = useDeleteUserNewsMutation();
-
-  const deleteItem = (id: string) => {
-    console.log("Delete item", id);
-    DeleteUserNewsMutation(id);
-  };
   return (
     <SectionPadding container>
       <div>
         <div className="mb-5 flex items-center">
           <h1 className="mr-auto">最新消息</h1>
           <Link href={`${createUserNewsUrl}/create`} className={buttonVariants({ variant: "donateNow" })}>
-            <FiPlus size={20} className="mr-2" />
+            <Icons.Add dimension="s" className="text-white" />
             新增資料
           </Link>
         </div>
         <div>
-          <ul className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
-            {Array.isArray(data) &&
-              data.map((item) => (
-                <li key={item.id} className="rounded-lg bg-white p-4 shadow-md">
-                  <div className="mb-4 flex items-center justify-between">
-                    <div className="text-lg font-bold">{item.title}</div>
-                  </div>
-                  <div className="mb-4 text-gray-600">發布日期：{format(item.publicAt!, "yyyy-MM-dd")}</div>
-                  <div className="flex items-center space-x-4">
-                    <div className="mr-auto flex">
-                      <Link
-                        className={cn("mr-3 text-primary", buttonVariants({ variant: "outline" }))}
-                        href={`${createUserNewsUrl}/${item.id}`}
-                      >
-                        <Pencil1Icon />
-                      </Link>
-                      <AlertDialogTriggerDelete deleteData={() => deleteItem(item.id)} title={item.title} />
-                    </div>
-                    <div className="flex items-center space-x-2">
-                      <div className="flex items-center space-x-2">
-                        <Checkbox checked={item.isActive} id={item.id} />
-                        <label
-                          htmlFor={item.id}
-                          className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                        >
-                          啟用
-                        </label>
-                      </div>
-                    </div>
-                  </div>
-                </li>
-              ))}
-          </ul>
+          <Suspense fallback={<Loading />}>
+            <NewsContent />
+          </Suspense>
         </div>
       </div>
     </SectionPadding>
