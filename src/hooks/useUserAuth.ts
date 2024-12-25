04ee58f8edeed9b0cffd04ba47b21879c572ab6f
Jethro <ttppoo12144@gmail.com>
2024-12-25 16:34:42 +1100

    refactor:登入 註冊  移除 toast 直接顯示訊息在頁面上

--- src/hooks/useUserAuth.ts
+++ src/hooks/useUserAuth.ts
@@ -1,31 +1,20 @@
 import { useMutation } from "@tanstack/react-query";
-import { useToast } from "./use-toast";
 import { signFlowApi } from "@/api/services/signFlow";
 
 export const useLogInMutation = () => {
-  const { toast } = useToast();
   return useMutation({
     mutationFn: signFlowApi.signIn,
     onError: (error) => {
       console.error("Error logging in user:", error.message);
-      toast({
-        variant: "destructive",
-        description: error.message,
-      });
     },
   });
 };
 
 export const useRegisterMutation = () => {
-  const { toast } = useToast();
   return useMutation({
     mutationFn: signFlowApi.signUp,
     onError: (error) => {
       console.error("Error logging in user:", error.message);
-      toast({
-        variant: "destructive",
-        description: error.message,
-      });
     },
   });
 };
