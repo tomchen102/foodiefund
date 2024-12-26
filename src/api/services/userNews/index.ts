90e0b3cae725afd4111bf3a4ef3843d8843a5299
Jethro <ttppoo12144@gmail.com>
2024-12-26 22:59:29 +1100

    feat:新增 getById 前後台切換的參數 用同一支就能處理前後台

--- src/api/services/userNews/index.ts
+++ src/api/services/userNews/index.ts
@@ -1,5 +1,6 @@
-import axiosClientFrontend from "@/api/axiosClientDashboard";
+import axiosClientDashboard from "@/api/axiosClientDashboard";
 import { UserNewsListResponseType } from "./types";
+import axiosClientFrontend from "@/api/axiosClientFrontend";
 
 export const userNewsApi = {
   getBaseUrl: (planId: string = "64c5ae5c6f2d3e001ccf9abc") => `/plan/${planId}/news`,
@@ -14,28 +15,29 @@ export const userNewsApi = {
     return formData;
   },
   getAll: async () => {
-    const response = await axiosClientFrontend.get(userNewsApi.getBaseUrl());
+    const response = await axiosClientDashboard.get(userNewsApi.getBaseUrl());
     return response.data;
   },
-  getById: async (id: string) => {
-    const response = await axiosClientFrontend.get(`${userNewsApi.getBaseUrl()}/${id}`);
+  getById: async (id: string, isFrontend: boolean = false) => {
+    const client = isFrontend ? axiosClientFrontend : axiosClientDashboard;
+    const response = await client.get(`${userNewsApi.getBaseUrl()}/${id}`);
     return response.data;
   },
   create: async (data: UserNewsListResponseType) => {
     userNewsApi.createFormData(data);
-    const response = await axiosClientFrontend.post(userNewsApi.getBaseUrl(), data, {
+    const response = await axiosClientDashboard.post(userNewsApi.getBaseUrl(), data, {
       headers: userNewsApi.MULTIPART_HEADERS,
     });
     return response.data;
   },
   update: async (data: UserNewsListResponseType) => {
     userNewsApi.createFormData(data);
-    const response = await axiosClientFrontend.put(`${userNewsApi.getBaseUrl()}/${data.id}`, data, {
+    const response = await axiosClientDashboard.put(`${userNewsApi.getBaseUrl()}/${data.id}`, data, {
       headers: userNewsApi.MULTIPART_HEADERS,
     });
     return response.data;
   },
   delete: async (id: string) => {
-    return axiosClientFrontend.delete(`${userNewsApi.getBaseUrl()}/${id}`);
+    return axiosClientDashboard.delete(`${userNewsApi.getBaseUrl()}/${id}`);
   },
 };
