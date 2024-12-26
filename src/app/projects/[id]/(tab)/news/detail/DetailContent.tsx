373aa21b2c678c5ed84bc9c34523b57ff820bf8b
Jethro <ttppoo12144@gmail.com>
2024-12-26 23:07:52 +1100

    feat:新增 DetailContent 獲取API 內容 和使用 html-react-parser 在這頁面處理 API的內容 獲取友好SEO

---
+++ src/app/projects/[id]/(tab)/news/detail/DetailContent.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import AnimatedContainer from "@/components/AnimatedContainer";
+import { useGetUserNewsId } from "@/hooks/uesUserNews";
+import { format } from "date-fns";
+import parse from "html-react-parser";
+import Image from "next/image";
+
+const DetailContent = ({ id }: { id: string }) => {
+  const { data, isLoading, isError } = useGetUserNewsId(id, { enabled: true }, true);
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError || !data) {
+    return <div>Error: Failed to load data.</div>;
+  }
+
+  return (
+    <AnimatedContainer>
+      <div className="pb-5">
+        <div className="border p-6">
+          <div className="mb-6">
+            <Image
+              src={typeof data.image === "string" ? data.image : ""}
+              width={0}
+              height={0}
+              sizes="100vw"
+              className="h-auto w-full object-contain"
+              alt={data.title}
+              priority
+            />
+          </div>
+          <h2 className="mb-2 text-lg font-bold md:text-2xl">計畫更新: {data.title}</h2>
+          <div className="mb-3">
+            <span className="text-gray">發佈日期：{format(new Date(data.publicAt ?? Date.now()), "yyyy-MM-dd")}</span>
+          </div>
+          <div className="prose mb-2">{parse(data.content ?? "")}</div>
+        </div>
+      </div>
+    </AnimatedContainer>
+  );
+};
+
+export default DetailContent;
