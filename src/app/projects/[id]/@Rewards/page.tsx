5f8676daf4821e2df7ad53e21110d97e7e70fb6d
Jethro <ttppoo12144@gmail.com>
2024-12-26 23:01:23 +1100

    refactor:重購next Image元件的處理方式  width={0} height={0} sizes="100vw"

--- src/app/projects/[id]/@Rewards/page.tsx
+++ src/app/projects/[id]/@Rewards/page.tsx
@@ -9,10 +9,6 @@ const Rewards = () => {
   const pathname = usePathname();
   const isProjectList = pathname.includes("/project-list");
   const { id } = useParams();
-  const ImageStyle = {
-    width: "100%",
-    height: "auto",
-  };
   const router = useRouter();
   const { user } = useAuth();
 
@@ -28,12 +24,12 @@ const Rewards = () => {
           <li className="mb-5 flex-shrink-0 md:mb-8 md:min-w-[200px]">
             <Image
               src="https://storage.googleapis.com/fir-express-80358.appspot.com/images/e84a603e-94ce-4955-aec1-57030925caf0.png?GoogleAccessId=firebase-adminsdk-42otj%40fir-express-80358.iam.gserviceaccount.com&Expires=16756675200&Signature=PYRZrOEf2DhIKnxV4IpWXrlIG1ms%2FfsYo%2FDFc5KJWDkTRadTSwDZHz7WKNuGQqzCF5Swgd8jl4M9uOmR62IoLtn5I50sq5Z8wZrP5YkHTnizbjS1%2Bte5S1egt2%2B5eNYFXTQ3861NLSScD%2F%2BoyNgYOkRU%2BpPs9JkmsEz0B9zhH5u%2BUCGoM8hZbOk%2FbQFjj%2FrigbY4ub9lwbWlJ8%2BNC8uQCc5Kdpniibh%2B8Sbb5MEc0ujkTVc0f1aWxfCZzl9eo%2BaXOVOKtS4ojjVVcL0MFdla5wFwS6YXOYVgLueuJ61gze%2F0D%2FSftMLSlei6Ij70xSp9j8s4%2F6LAJfFCABOKjo8MYA%3D%3D"
-              width={306}
-              height={200}
+              width={0}
+              height={0}
+              sizes="100vw"
               className="h-auto w-full xl:h-[200px]"
               alt="Screenshots of the dashboard project showing desktop version"
-              loading="lazy"
-              style={ImageStyle}
+              priority
             />
             <div className="border p-5">
               <h3 className="mb-2 text-base">【限量單品】布偶貓托特包</h3>
@@ -61,12 +57,12 @@ const Rewards = () => {
           <li className="mb-5 flex-shrink-0 md:mb-8 md:min-w-[200px]">
             <Image
               src="https://storage.googleapis.com/fir-express-80358.appspot.com/images/d85d3add-863c-414b-a68c-2e2867035fc7.png?GoogleAccessId=firebase-adminsdk-42otj%40fir-express-80358.iam.gserviceaccount.com&Expires=16756675200&Signature=WLGOtvhCALB16Cvbt%2FJ7Qjs3r9DjlbdM1xpvMKPNskP38pCkr3LsNANPTLGXDLdfv3RVAQiZo6VRLE%2B3V3kZB0kKWSzKFo%2BxgTbvKg5PYf7F3PBxzoqxxmkQ3Mn7hyvxL9SPtDpJne9aW%2FsycIs7fyy85XZzOxMKpTzOw4o3%2B54pmYSqpCzJQJu2FYqe10kYzZDjf1X3Je5IoUzCzSEDPaU56VEAb7YrNShzB0skkRdk443oRzKryCSXd%2BixJPvO%2FxvxYm7LOvB77lTq1fXiTT56hvN6N90DdCKZACBBMoJHyKBk%2FMZkC5rGy20IR9RVVl5ftuctp03wInFm9nF2zA%3D%3D"
-              width={306}
-              height={200}
+              width={0}
+              height={0}
+              sizes="100vw"
               className="h-auto w-full xl:h-[200px]"
               alt="Screenshots of the dashboard project showing desktop version"
-              loading="lazy"
-              style={ImageStyle}
+              priority
             />
             <div className="border p-5">
               <h3 className="mb-2 text-base">【限量單品】布偶貓托特包</h3>
@@ -94,12 +90,12 @@ const Rewards = () => {
           <li className="mb-5 flex-shrink-0 md:mb-8 md:min-w-[200px]">
             <Image
               src="https://storage.googleapis.com/fir-express-80358.appspot.com/images/df262d64-4ecb-4f6c-9c8f-2f0cb79e09b0.png?GoogleAccessId=firebase-adminsdk-42otj%40fir-express-80358.iam.gserviceaccount.com&Expires=16756675200&Signature=GlgbjbN7zWBpvmw%2BK7xp6qLOaTWKSdQtKdGTS0Zm5EF1TaqXl2lWO6A9EML2aOp0Ye6uN5FSr0HKYuVrdv5wmYEDivjdAwUeaRSyF0RD2nGS67TlqVP%2FSKVpjs7QMO1PDd2elyIOgSKtg8GpJRQCGRYZA4oNmAVE%2FOhLgAL8%2B1v1fYpAf0HiDaYZSiY21FSZB%2BwQUxy3hnNtY3VCEqal%2BEt940jsAeBYIQ9%2Bb4kmb%2B26SfKh%2FNw3D8VetnyiT6WBfMt5vTnAZCI%2Bb%2FVa0f9JkBg6qLAgFynWP2hUIQCVR3hJSvjgnBnEtDNZmrQnwmSpk%2BsObS8oeusgIZFiqd77Wg%3D%3D"
-              width={306}
-              height={200}
+              width={0}
+              height={0}
+              sizes="100vw"
               className="h-auto w-full xl:h-[200px]"
               alt="Screenshots of the dashboard project showing desktop version"
-              loading="lazy"
-              style={ImageStyle}
+              priority
             />
             <div className="border p-5">
               <h3 className="mb-2 text-base">【限量單品】布偶貓托特包</h3>
@@ -127,12 +123,12 @@ const Rewards = () => {
           <li className="mb-5 flex-shrink-0 md:mb-8 md:min-w-[200px]">
             <Image
               src="https://storage.googleapis.com/fir-express-80358.appspot.com/images/e84a603e-94ce-4955-aec1-57030925caf0.png?GoogleAccessId=firebase-adminsdk-42otj%40fir-express-80358.iam.gserviceaccount.com&Expires=16756675200&Signature=PYRZrOEf2DhIKnxV4IpWXrlIG1ms%2FfsYo%2FDFc5KJWDkTRadTSwDZHz7WKNuGQqzCF5Swgd8jl4M9uOmR62IoLtn5I50sq5Z8wZrP5YkHTnizbjS1%2Bte5S1egt2%2B5eNYFXTQ3861NLSScD%2F%2BoyNgYOkRU%2BpPs9JkmsEz0B9zhH5u%2BUCGoM8hZbOk%2FbQFjj%2FrigbY4ub9lwbWlJ8%2BNC8uQCc5Kdpniibh%2B8Sbb5MEc0ujkTVc0f1aWxfCZzl9eo%2BaXOVOKtS4ojjVVcL0MFdla5wFwS6YXOYVgLueuJ61gze%2F0D%2FSftMLSlei6Ij70xSp9j8s4%2F6LAJfFCABOKjo8MYA%3D%3D"
-              width={306}
-              height={200}
+              width={0}
+              height={0}
+              sizes="100vw"
               className="h-auto w-full xl:h-[200px]"
               alt="Screenshots of the dashboard project showing desktop version"
-              loading="lazy"
-              style={ImageStyle}
+              priority
             />
             <div className="border p-5">
               <h3 className="mb-2 text-base">【限量單品】布偶貓托特包</h3>
@@ -163,10 +159,10 @@ const Rewards = () => {
           <li className="mb-5 md:mb-8">
             <Image
               src="https://storage.googleapis.com/fir-express-80358.appspot.com/images/e84a603e-94ce-4955-aec1-57030925caf0.png?GoogleAccessId=firebase-adminsdk-42otj%40fir-express-80358.iam.gserviceaccount.com&Expires=16756675200&Signature=PYRZrOEf2DhIKnxV4IpWXrlIG1ms%2FfsYo%2FDFc5KJWDkTRadTSwDZHz7WKNuGQqzCF5Swgd8jl4M9uOmR62IoLtn5I50sq5Z8wZrP5YkHTnizbjS1%2Bte5S1egt2%2B5eNYFXTQ3861NLSScD%2F%2BoyNgYOkRU%2BpPs9JkmsEz0B9zhH5u%2BUCGoM8hZbOk%2FbQFjj%2FrigbY4ub9lwbWlJ8%2BNC8uQCc5Kdpniibh%2B8Sbb5MEc0ujkTVc0f1aWxfCZzl9eo%2BaXOVOKtS4ojjVVcL0MFdla5wFwS6YXOYVgLueuJ61gze%2F0D%2FSftMLSlei6Ij70xSp9j8s4%2F6LAJfFCABOKjo8MYA%3D%3D"
-              width={306}
-              height={200}
-              className="h-full w-full xl:h-[200px]"
-              style={ImageStyle}
+              width={0}
+              height={0}
+              sizes="100vw"
+              className="h-auto w-full xl:h-[200px]"
               alt="Screenshots of the dashboard project showing desktop version"
               priority
             />
@@ -196,10 +192,10 @@ const Rewards = () => {
           <li className="mb-5 md:mb-8">
             <Image
               src="https://storage.googleapis.com/fir-express-80358.appspot.com/images/d85d3add-863c-414b-a68c-2e2867035fc7.png?GoogleAccessId=firebase-adminsdk-42otj%40fir-express-80358.iam.gserviceaccount.com&Expires=16756675200&Signature=WLGOtvhCALB16Cvbt%2FJ7Qjs3r9DjlbdM1xpvMKPNskP38pCkr3LsNANPTLGXDLdfv3RVAQiZo6VRLE%2B3V3kZB0kKWSzKFo%2BxgTbvKg5PYf7F3PBxzoqxxmkQ3Mn7hyvxL9SPtDpJne9aW%2FsycIs7fyy85XZzOxMKpTzOw4o3%2B54pmYSqpCzJQJu2FYqe10kYzZDjf1X3Je5IoUzCzSEDPaU56VEAb7YrNShzB0skkRdk443oRzKryCSXd%2BixJPvO%2FxvxYm7LOvB77lTq1fXiTT56hvN6N90DdCKZACBBMoJHyKBk%2FMZkC5rGy20IR9RVVl5ftuctp03wInFm9nF2zA%3D%3D"
-              width={306}
-              height={200}
+              width={0}
+              height={0}
+              sizes="100vw"
               className="h-auto w-full xl:h-[200px]"
-              style={ImageStyle}
               alt="Screenshots of the dashboard project showing desktop version"
               priority
             />
@@ -229,10 +225,10 @@ const Rewards = () => {
           <li>
             <Image
               src="https://storage.googleapis.com/fir-express-80358.appspot.com/images/df262d64-4ecb-4f6c-9c8f-2f0cb79e09b0.png?GoogleAccessId=firebase-adminsdk-42otj%40fir-express-80358.iam.gserviceaccount.com&Expires=16756675200&Signature=GlgbjbN7zWBpvmw%2BK7xp6qLOaTWKSdQtKdGTS0Zm5EF1TaqXl2lWO6A9EML2aOp0Ye6uN5FSr0HKYuVrdv5wmYEDivjdAwUeaRSyF0RD2nGS67TlqVP%2FSKVpjs7QMO1PDd2elyIOgSKtg8GpJRQCGRYZA4oNmAVE%2FOhLgAL8%2B1v1fYpAf0HiDaYZSiY21FSZB%2BwQUxy3hnNtY3VCEqal%2BEt940jsAeBYIQ9%2Bb4kmb%2B26SfKh%2FNw3D8VetnyiT6WBfMt5vTnAZCI%2Bb%2FVa0f9JkBg6qLAgFynWP2hUIQCVR3hJSvjgnBnEtDNZmrQnwmSpk%2BsObS8oeusgIZFiqd77Wg%3D%3D"
-              width={306}
-              height={200}
+              width={0}
+              height={0}
+              sizes="100vw"
               className="h-auto w-full xl:h-[200px]"
-              style={ImageStyle}
               alt="Screenshots of the dashboard project showing desktop version"
               priority
             />
--- src/app/projects/[id]/project-list/payment/_components/CheckoutList/index.tsx
+++ src/app/projects/[id]/project-list/payment/_components/CheckoutList/index.tsx
@@ -1,18 +1,14 @@
 import Image from "next/image";
 
 const CheckoutList = () => {
-  const ImageStyle = {
-    width: "100%",
-    height: "auto",
-  };
   return (
     <div className="mb-5 md:mb-8">
       <Image
         src="https://storage.googleapis.com/fir-express-80358.appspot.com/images/e84a603e-94ce-4955-aec1-57030925caf0.png?GoogleAccessId=firebase-adminsdk-42otj%40fir-express-80358.iam.gserviceaccount.com&Expires=16756675200&Signature=PYRZrOEf2DhIKnxV4IpWXrlIG1ms%2FfsYo%2FDFc5KJWDkTRadTSwDZHz7WKNuGQqzCF5Swgd8jl4M9uOmR62IoLtn5I50sq5Z8wZrP5YkHTnizbjS1%2Bte5S1egt2%2B5eNYFXTQ3861NLSScD%2F%2BoyNgYOkRU%2BpPs9JkmsEz0B9zhH5u%2BUCGoM8hZbOk%2FbQFjj%2FrigbY4ub9lwbWlJ8%2BNC8uQCc5Kdpniibh%2B8Sbb5MEc0ujkTVc0f1aWxfCZzl9eo%2BaXOVOKtS4ojjVVcL0MFdla5wFwS6YXOYVgLueuJ61gze%2F0D%2FSftMLSlei6Ij70xSp9j8s4%2F6LAJfFCABOKjo8MYA%3D%3D"
-        width={306}
-        height={200}
+        width={0}
+        height={0}
+        sizes="100vw"
         className="h-full w-full xl:h-[200px]"
-        style={ImageStyle}
         alt="Screenshots of the dashboard project showing desktop version"
         priority
       />
