317f592940408e8d6800930aaa2fc8371dd9a1a2
Jethro <ttppoo12144@gmail.com>
2024-12-30 22:12:34 +1100

    feat:新增 前台 faq的api 使用 串真實資料

---
+++ src/app/projects/[id]/(tab)/faq/FaqContent.tsx
@@ -0,0 +1,33 @@
+"use client";
+import { format } from "date-fns";
+
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
+import { useGetUserFaq } from "@/hooks/useUserFaq";
+
+const FaqContent = () => {
+  const { data, isLoading, isError } = useGetUserFaq("frontend");
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError || !data) {
+    return <div>Error: Failed to load data.</div>;
+  }
+
+  return (
+    <Accordion type="single" collapsible className="w-full">
+      {data.map((item) => (
+        <AccordionItem key={item.id} className="mb-5 border-l border-r border-t p-6" value={item.id}>
+          <span className="mb-1 block text-gray">更新於 {format(new Date(item.updatedAt!), "yyyy-MM-dd")}</span>
+          <AccordionTrigger className="hover:text-primary-dark hover:no-underline md:text-2xl">
+            {item.questions}
+          </AccordionTrigger>
+          <AccordionContent className="md:text-xl">{item.answers}</AccordionContent>
+        </AccordionItem>
+      ))}
+    </Accordion>
+  );
+};
+
+export default FaqContent;
