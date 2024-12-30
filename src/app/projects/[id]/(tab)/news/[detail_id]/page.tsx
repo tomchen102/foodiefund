bf9bf3e46990076bea2fc4ce25b780718b579e19
Jethro <ttppoo12144@gmail.com>
2024-12-30 22:14:30 +1100

    feat:前台 專案底下的最新消息 詳情 API 串真實API

--- src/app/projects/[id]/(tab)/news/[detail_id]/page.tsx
+++ src/app/projects/[id]/(tab)/news/[detail_id]/page.tsx
@@ -1,13 +1,13 @@
 import { HydrationBoundary } from "@tanstack/react-query";
 
-import { initializeQueryNewsClient } from "@/hooks/uesUserNews";
+import { initializeNewsByIdQueryClient } from "@/hooks/uesUserNews";
 
 import DetailContent from "./DetailContent";
 
 const DUMMY_ID = "676cdded66e5be04cc208901";
 
 const getData = async () => {
-  const { dehydratedState } = await initializeQueryNewsClient(DUMMY_ID);
+  const { dehydratedState } = await initializeNewsByIdQueryClient(DUMMY_ID);
   return {
     dehydratedState,
   };
@@ -18,7 +18,7 @@ const Detail = async () => {
 
   return (
     <HydrationBoundary state={dehydratedState}>
-      <DetailContent id={DUMMY_ID} />
+      <DetailContent />
     </HydrationBoundary>
   );
 };
