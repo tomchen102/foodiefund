33811418fcee72636e04a72ed47e5fef741ecbe0
chindesu0207 <potter0207@gmail.com>
2025-01-02 11:24:31 +0800

    refactor: 調整尺寸並替換 icon

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -1,8 +1,7 @@
 "use client";
-
 import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
-import { CheckIcon } from "@radix-ui/react-icons";
 import * as React from "react";
+import { MdCheck } from "react-icons/md";
 
 import { cn } from "@/lib/utils";
 
@@ -13,13 +12,13 @@ const Checkbox = React.forwardRef<
   <CheckboxPrimitive.Root
     ref={ref}
     className={cn(
-      "peer h-4 w-4 shrink-0 rounded-sm border border-primary shadow focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground",
+      "peer h-6 w-6 shrink-0 rounded-sm border-2 border-gray-200 shadow focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:border-primary-dark data-[state=checked]:bg-primary-dark data-[state=checked]:text-primary-foreground",
       className
     )}
     {...props}
   >
     <CheckboxPrimitive.Indicator className={cn("flex items-center justify-center text-current")}>
-      <CheckIcon className="h-4 w-4" />
+      <MdCheck />
     </CheckboxPrimitive.Indicator>
   </CheckboxPrimitive.Root>
 ));
--- src/components/ui/radio-group.tsx
+++ src/components/ui/radio-group.tsx
@@ -1,8 +1,7 @@
 "use client";
-
-import { CheckIcon } from "@radix-ui/react-icons";
 import * as RadioGroupPrimitive from "@radix-ui/react-radio-group";
 import * as React from "react";
+import { MdCircle } from "react-icons/md";
 
 import { cn } from "@/lib/utils";
 
@@ -22,13 +21,13 @@ const RadioGroupItem = React.forwardRef<
     <RadioGroupPrimitive.Item
       ref={ref}
       className={cn(
-        "aspect-square h-4 w-4 rounded-full border border-primary text-primary shadow focus:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+        "aspect-square h-6 w-6 rounded-full border-2 border-gray-200 text-primary-dark shadow focus:outline-none focus-visible:ring-2 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:border-primary-dark",
         className
       )}
       {...props}
     >
       <RadioGroupPrimitive.Indicator className="flex items-center justify-center">
-        <CheckIcon className="h-3.5 w-3.5 fill-primary" />
+        <MdCircle />
       </RadioGroupPrimitive.Indicator>
     </RadioGroupPrimitive.Item>
   );
