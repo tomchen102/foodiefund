0a3c62b3f4128441d8f0bb0b833a570f5489909a
chindesu0207 <potter0207@gmail.com>
2025-01-02 12:30:18 +0800

    fix: 修復 Logo 圖示不會旋轉的問題

--- src/components/Home/Logo/index.tsx
+++ src/components/Home/Logo/index.tsx
@@ -11,13 +11,17 @@ const Logo = ({ darkMode = false }: modeProps) => {
   return (
     <Link
       href="/"
-      className={cn("mr-auto flex items-center font-bold", darkMode ? "gap-3 text-2xl text-white" : "gap-2 text-base")}
+      className={cn(
+        "logo-link mr-auto flex items-center font-bold",
+        darkMode ? "gap-3 text-2xl text-white" : "gap-2 text-base"
+      )}
     >
       <div className={cn("relative inline-block", darkMode ? "size-8" : "size-6")}>
         <Image
           src="https://firebasestorage.googleapis.com/v0/b/foodiefund-7b103.appspot.com/o/logo.png?alt=media&token=50bbd619-ec49-441c-9030-501c4826fb69"
           fill
           sizes="32px"
+          className="logo"
           alt="FoodieFund logo"
         />
       </div>
