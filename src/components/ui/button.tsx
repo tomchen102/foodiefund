789a26bf9238ea38c69ddf82e8a94fbbcf95a65f
chindesu0207 <potter0207@gmail.com>
2025-01-02 11:28:08 +0800

    refactor: 調整顏色及大小設定，size 新增 iconButton 設定

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,26 +5,28 @@ import * as React from "react";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-base font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
-        default: "bg-primary text-block shadow hover:text-white",
+        default: "bg-primary text-primary-dark font-bold shadow hover:text-white hover:bg-primary-hover",
         destructive: "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
         outline: "border border-gray bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
-        secondary: "bg-secondary text-secondary-foreground shadow-sm text-white text-sm font-bold leading-[20px]",
+        secondary:
+          "bg-secondary text-secondary-foreground shadow-sm text-white font-bold leading-[20px] hover:bg-secondary-hover",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
         donateNow: "bg-secondary text-base font-bold text-white",
         transparent:
-          "border border-white bg-transparent shadow-sm hover:bg-transparent/40 text-white text-sm font-bold leading-[20px]",
+          "border border-white bg-transparent shadow-sm hover:bg-transparent/40 text-white  font-bold leading-[20px]",
         bannerTag: "text-lg font-bold md:text-xl",
       },
       size: {
-        default: "h-9 px-4 py-2 rounded",
+        default: "h-9 px-5 py-3 rounded",
         sm: "h-8 rounded-md px-3 text-xs",
         lg: "h-10 rounded py-6 w-full",
-        icon: "h-12 w-12",
+        icon: "h-9 w-9",
+        iconButton: "py-1 pl-3 pr-2 rounded",
       },
     },
     defaultVariants: {
