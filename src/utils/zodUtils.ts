8b5064cd5ed970a6b07be82981228915193c39aa
Jethro <ttppoo12144@gmail.com>
2025-01-12 02:46:04 +1100

    refactor: 修改錯誤訊息的字串

--- src/utils/zodUtils.ts
+++ src/utils/zodUtils.ts
@@ -4,7 +4,7 @@ export const safeParseResponse = <T, U>(schema: ZodSchema<T>, response: U): T =>
   const result = schema.safeParse(response);
   if (!result.success) {
     console.error("資料格式無效zod", result.error);
-    throw new Error("資料格式無效");
+    throw new Error("目前資料有狀況，請重新整理");
   }
   return result.data;
 };
