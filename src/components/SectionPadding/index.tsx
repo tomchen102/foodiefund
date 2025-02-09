82c2af676c6c93dff8a5652072a09febe2932bf3
Jethro <ttppoo12144@gmail.com>
2025-02-09 20:58:22 +1100

    feat:新增 SectionPadding 元件 adminContainer的屬性 給後台使用

--- src/components/SectionPadding/index.tsx
+++ src/components/SectionPadding/index.tsx
@@ -1,12 +1,13 @@
 import { cn } from "@/lib/utils";
 import { SectionPaddingProps } from "@/types/ProviderType";
 
-const SectionPadding = ({ children, className, container }: SectionPaddingProps) => {
+const SectionPadding = ({ children, className, container, adminContainer }: SectionPaddingProps) => {
   return (
     <section
       className={cn(
         "pb-8 pt-6 lg:gap-6 lg:pb-[120px] lg:pt-[100px]",
         container ? "container px-3 xl:px-0" : "",
+        adminContainer ? "lg:py-10" : "",
         className
       )}
     >
