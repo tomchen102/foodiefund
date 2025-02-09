3bd62f415060f4c9f8660bc7f9410db5dd04f09f
Jethro <ttppoo12144@gmail.com>
2025-02-09 20:56:48 +1100

    feat:新增 後台 計畫的select 拉出元件直接管理 計畫資料

---
+++ src/app/dashboard/_components/PlanSelect/index.tsx
@@ -0,0 +1,36 @@
+import Badges from "@/components/Badges";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+
+const navMenu = {
+  projectNav: [
+    { title: "金華火腿主題咖啡屋", hrefUrl: "#" },
+    { title: "喵喵咖啡廳", hrefUrl: "#" },
+    { title: "龍蝦盛宴", hrefUrl: "#" },
+  ],
+};
+const PlanSelect = () => {
+  return (
+    <div className="mb-10 flex items-center">
+      <div className="mr-6">
+        <h5>募資監控中心</h5>
+      </div>
+      <Select>
+        <SelectTrigger className="w-[192px]">
+          <SelectValue placeholder="金華火腿主題咖啡屋" />
+        </SelectTrigger>
+        <SelectContent>
+          {navMenu.projectNav.map((item, index) => (
+            <SelectItem key={index} value={item.title}>
+              {item.title}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+      <div className="ml-3">
+        <Badges text="審核中" />
+      </div>
+    </div>
+  );
+};
+
+export default PlanSelect;
