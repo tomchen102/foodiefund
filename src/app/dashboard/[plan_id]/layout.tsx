c13f443a7537737acf7e64b7ed4a7d1e5d0b17f5
Jethro <ttppoo12144@gmail.com>
2025-02-09 20:52:54 +1100

    refactor:把計畫select 拉出元件處理 並且把SectionPadding新增 adminContainer

--- src/app/dashboard/[plan_id]/layout.tsx
+++ src/app/dashboard/[plan_id]/layout.tsx
@@ -1,38 +1,13 @@
 import SectionPadding from "@/components/SectionPadding";
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { ProviderProps } from "@/types/ProviderType";
 
 import LayoutContent from "./content";
 
-const navMenu = {
-  projectNav: [
-    { title: "金華火腿主題咖啡屋", hrefUrl: "#" },
-    { title: "喵喵咖啡廳", hrefUrl: "#" },
-    { title: "龍蝦盛宴", hrefUrl: "#" },
-  ],
-};
 const ProjectsLayout = (props: ProviderProps) => {
   const { children } = props;
 
   return (
-    <SectionPadding container>
-      <div className="lg:grid lg:grid-cols-12 lg:gap-6">
-        <div className="lg:col-span-3"></div>
-        <div className="mb-5">
-          <Select>
-            <SelectTrigger className="w-[255px]">
-              <SelectValue placeholder="金華火腿主題咖啡屋" />
-            </SelectTrigger>
-            <SelectContent>
-              {navMenu.projectNav.map((item, index) => (
-                <SelectItem key={index} value={item.title}>
-                  {item.title}
-                </SelectItem>
-              ))}
-            </SelectContent>
-          </Select>
-        </div>
-      </div>
+    <SectionPadding container adminContainer>
       <LayoutContent>{children}</LayoutContent>
     </SectionPadding>
   );
