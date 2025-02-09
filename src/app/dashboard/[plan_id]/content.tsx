ca4522fed44bc5975b08165c9931e08bba8c5365
Jethro <ttppoo12144@gmail.com>
2025-02-09 20:02:46 +1100

    style:修正排版 按照設計圖的排版

--- src/app/dashboard/[plan_id]/content.tsx
+++ src/app/dashboard/[plan_id]/content.tsx
@@ -1,18 +1,22 @@
 "use client";
+import { ProviderProps } from "@/types/ProviderType";
+
 import Navbar from "../_components/Navbar";
 import MobileNavbar from "../_components/Navbar/MobileNavbar";
+import PlanSelect from "../_components/PlanSelect";
 
-const LayoutContent = ({ children }: { children: React.ReactNode }) => {
+const LayoutContent = ({ children }: ProviderProps) => {
   return (
-    <>
-      <div className="lg:grid lg:grid-cols-12 lg:gap-6">
-        <div className="lg:col-span-3">
-          <Navbar />
-          <MobileNavbar />
-        </div>
-        <div className="lg:col-span-9">{children}</div>
+    <div className="flex">
+      <div className="mr-8">
+        <Navbar />
+        <MobileNavbar />
       </div>
-    </>
+      <div>
+        <PlanSelect />
+        {children}
+      </div>
+    </div>
   );
 };
 
