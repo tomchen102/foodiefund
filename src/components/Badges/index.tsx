7222286b8fa963717315c18a9def58f48a14a20d
Jethro <ttppoo12144@gmail.com>
2025-02-09 20:57:37 +1100

    feat:新增 審核中 文字 並且每個文字對應相對應的style

--- src/components/Badges/index.tsx
+++ src/components/Badges/index.tsx
@@ -3,20 +3,20 @@ import { LiteralUnion } from "react-hook-form";
 import { cn } from "@/lib/utils";
 
 interface BadgesProps {
-  text: LiteralUnion<"募資中" | "台北" | "訂閱式", string>;
+  text: LiteralUnion<"募資中" | "台北" | "訂閱式" | "審核中", string>;
   className?: string;
 }
+
+const badgeStyles: Record<string, string> = {
+  募資中: "bg-secondary-light text-secondary",
+  台北: "bg-secondary-light text-secondary",
+  訂閱式: "bg-primary-light text-primary-dark",
+  審核中: "bg-[#F8D4D4] text-[#DC2626]",
+};
+
 const Badges = ({ text, className }: BadgesProps) => {
   return (
-    <span
-      className={cn(
-        `mr-3 inline-block rounded-sm px-2 py-1 font-bold`,
-        text === "訂閱式" ? "bg-primary-light text-primary-dark" : "bg-secondary-light text-secondary",
-        className
-      )}
-    >
-      {text}
-    </span>
+    <span className={cn(`mr-3 inline-block rounded-sm px-2 py-1 text-xs`, badgeStyles[text], className)}>{text}</span>
   );
 };
 
