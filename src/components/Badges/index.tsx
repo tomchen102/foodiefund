e5770707e7338c12b02b36767c8735d7945bb0db
Jethro <ttppoo12144@gmail.com>
2025-02-23 19:59:17 +1100

    feat:新增 咖啡廳 付款成功 退款中 退款成功 未讀 屬性 顯示不同顏色

--- src/components/Badges/index.tsx
+++ src/components/Badges/index.tsx
@@ -3,7 +3,10 @@ import { LiteralUnion } from "react-hook-form";
 import { cn } from "@/lib/utils";
 
 interface BadgesProps {
-  text: LiteralUnion<"募資中" | "台北" | "訂閱式" | "審核中", string>;
+  text: LiteralUnion<
+    "募資中" | "台北" | "訂閱式" | "審核中" | "咖啡廳" | "付款成功" | "退款中" | "退款成功" | "未讀",
+    string
+  >;
   className?: string;
 }
 
@@ -12,6 +15,11 @@ const badgeStyles: Record<string, string> = {
   台北: "bg-secondary-light text-secondary",
   訂閱式: "bg-primary-light text-primary-dark",
   審核中: "bg-[#F8D4D4] text-[#DC2626]",
+  咖啡廳: "bg-secondary-light text-secondary",
+  付款成功: "bg-[#e6f4f0] text-[#059669] border-[#82CBB4] border",
+  退款中: "bg-[#FBE9E9] text-[#DC2626] border-[#F8D4D4] border",
+  退款成功: "bg-[#E6F3F9] text-[#0284C7] border-[#80C1E3] border",
+  未讀: "bg-[#FBE9E9] text-[#DC2626] border-[#F8D4D4] border",
 };
 
 const Badges = ({ text, className }: BadgesProps) => {
