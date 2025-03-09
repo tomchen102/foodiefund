04cc533d140349318f2742a29b610aeb4a8c3e49
chindesu0207 <potter0207@gmail.com>
2025-03-09 12:59:24 +0800

    refactor: 新增 planApi create 相關的部分設置

--- src/api/services/plan/types.ts
+++ src/api/services/plan/types.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
 
-import { planProject, planSteps } from "@/schema/planSchema";
+import { planProject, planSteps, PlanFormSchema } from "@/schema/planSchema";
 
 export type planStepsType = z.infer<typeof planSteps.element>;
 export type planProjectType = z.infer<typeof planProject>;
+export type PlanFormSchemaType = z.infer<typeof PlanFormSchema>;
